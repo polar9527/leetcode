cdb26c150cc853b460295274a067cc1b21b20b89
polar <polar9527@163.com>
2018-11-29 17:50:57 +0800

    refactor folders

---
+++ 1_TwoSum/Golang/TwoSum.go
@@ -0,0 +1,17 @@
+func twoSum(nums []int, target int) []int {
+	dict := make(map[int]int)
+	var result []int
+	for index, num := range nums {
+		// fmt.Println(num)
+		complement := target - num
+		if _, ok := dict[complement]; !ok {
+			dict[num] = index
+			continue
+		}
+		result := []int{dict[complement], index}
+		// fmt.Println("in for")
+		return result
+	}
+	// fmt.Println("out for")
+	return result
+}
