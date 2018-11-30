c29f5f33bf888153b5419a66209d797230f0e26a
polar <polar9527@163.com>
2018-11-30 09:44:01 +0800

    remove Printf

--- 1_TwoSum/Golang/TwoSum.go
+++ 1_TwoSum/Golang/TwoSum.go
@@ -1,22 +1,20 @@
 package twosum
 
-import "fmt"
-
 // TwoSum solution of leetcode No.1 problem
 func TwoSum(nums []int, target int) []int {
 	dict := make(map[int]int)
 	var result []int
 	for index, num := range nums {
-		fmt.Println(num)
+		// fmt.Println(num)
 		complement := target - num
 		if _, ok := dict[complement]; !ok {
 			dict[num] = index
 			continue
 		}
 		result := []int{dict[complement], index}
-		fmt.Println("in for")
+		// fmt.Println("in for")
 		return result
 	}
-	fmt.Println("out for")
+	// fmt.Println("out for")
 	return result
 }
