874f1ec9e084e04a97a44dd45efe7b8db50eb9cb
polar <polar9527@163.com>
2018-11-30 18:02:08 +0800

    prepare unittest and refactor profile for python solution of problem 26

--- 26_RemoveDuplicatesFromSortedArray/Python/26_RemoveDuplicatesFromSortedArray.py
+++ 26_RemoveDuplicatesFromSortedArray/Python/26_RemoveDuplicatesFromSortedArray.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import timeit
+import unittest
 
-nums = [1, 1, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7]
 
-
-def removeDuplicatesA(nums):
+def removeDuplicates(nums):
     if len(nums) == 0:
         return 0
 
@@ -19,21 +18,29 @@ def removeDuplicatesA(nums):
     return slower + 1
 
 
-def removeDuplicatesB(nums):
+def removeDuplicatesBuildin(nums):
     nums[:] = sorted(list(set(nums)))
     return len(nums)
 
 
-rep = 8
-# print(timeit.timeit("testA()", setup="from __main__ import testA", number=1))
+nums = [1, 1, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7]
 
-print(timeit.repeat("removeDuplicatesA(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
-                                                     "from __main__ import removeDuplicatesA",
-                    repeat=rep))
 
-print(timeit.repeat("removeDuplicatesB(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
-                                                     "from __main__ import removeDuplicatesB",
-                    repeat=rep))
+class TestRemoveDuplicates(unittest.TestCase):
+    pass
 
-# [1.7508393086419753, 1.7301957530864196, 1.7445479506172838, 1.711339851851852, 2.061765135802469, 1.8548669629629622, 1.7146026666666678, 1.7883930864197524]
-# [1.2516025679012355, 1.2129351111111113, 1.1901202962962962, 1.2308744691358022, 1.2144853333333323, 1.1707535802469131, 1.2358640987654326, 1.150896592592595]
+
+if __name__ == '__main__':
+    rep = 8
+    # print(timeit.timeit("testA()", setup="from __main__ import testA", number=1))
+
+    print(timeit.repeat("removeDuplicatesA(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
+                        "from __main__ import removeDuplicatesA",
+                        repeat=rep))
+
+    print(timeit.repeat("removeDuplicatesB(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
+                        "from __main__ import removeDuplicatesB",
+                        repeat=rep))
+
+    # [1.7508393086419753, 1.7301957530864196, 1.7445479506172838, 1.711339851851852, 2.061765135802469, 1.8548669629629622, 1.7146026666666678, 1.7883930864197524]
+    # [1.2516025679012355, 1.2129351111111113, 1.1901202962962962, 1.2308744691358022, 1.2144853333333323, 1.1707535802469131, 1.2358640987654326, 1.150896592592595]
