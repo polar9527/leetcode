35d1c1b08b519c9fa08f99a894e065228a18fff5
polar9527 <polar9527@163.com>
2018-12-03 17:24:28 +0800

    unittest  for python solution of problem 26

--- 26_RemoveDuplicatesFromSortedArray/Python/26_RemoveDuplicatesFromSortedArray.py
+++ 26_RemoveDuplicatesFromSortedArray/Python/26_RemoveDuplicatesFromSortedArray.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import timeit
 import unittest
+import timeit
 
 
 def removeDuplicates(nums):
@@ -23,24 +23,40 @@ def removeDuplicatesBuildin(nums):
     return len(nums)
 
 
-nums = [1, 1, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7]
-
-
 class TestRemoveDuplicates(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.nums = [1, 1, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7]
+        self.result = 6
+
+    def test_removeDuplicates(self):
+        result = removeDuplicates(self.nums)
+        self.assertEqual(self.result, result)
+
+
+class TestRemoveDuplicatesBuildin(unittest.TestCase):
+
+    def setUp(self):
+        self.nums = [1, 1, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7]
+        self.result = 6
+
+    def test_removeDuplicatesBuildin(self):
+        result = removeDuplicatesBuildin(self.nums)
+        self.assertEqual(self.result, result)
 
 
 if __name__ == '__main__':
-    rep = 8
+    unittest.main(verbosity=2)
+    # rep = 8
     # print(timeit.timeit("testA()", setup="from __main__ import testA", number=1))
 
-    print(timeit.repeat("removeDuplicatesA(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
-                        "from __main__ import removeDuplicatesA",
-                        repeat=rep))
+    # print(timeit.repeat("removeDuplicates(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
+    #                     "from __main__ import removeDuplicates",
+    #                     repeat=rep))
 
-    print(timeit.repeat("removeDuplicatesB(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
-                        "from __main__ import removeDuplicatesB",
-                        repeat=rep))
+    # print(timeit.repeat("removeDuplicatesBuildin(nums)", setup="nums = [1,1,3,4,4,5,5,5,6,7,7,7];"
+    #                     "from __main__ import removeDuplicatesBuildin",
+    #                     repeat=rep))
 
     # [1.7508393086419753, 1.7301957530864196, 1.7445479506172838, 1.711339851851852, 2.061765135802469, 1.8548669629629622, 1.7146026666666678, 1.7883930864197524]
     # [1.2516025679012355, 1.2129351111111113, 1.1901202962962962, 1.2308744691358022, 1.2144853333333323, 1.1707535802469131, 1.2358640987654326, 1.150896592592595]
