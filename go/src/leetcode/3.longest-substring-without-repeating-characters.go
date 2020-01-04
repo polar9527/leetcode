af79a666663dec5c7e2d0ec56e8322e7a7eaedaf
polar <polar9527@163.com>
2020-01-04 19:39:29 +0800

    go:3

--- go/src/leetcode/3.longest-substring-without-repeating-characters.go
+++ go/src/leetcode/3.longest-substring-without-repeating-characters.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"math"
+	"strings"
 )
 
 func lengthOfLongestSubstring(s string) int {
@@ -28,16 +29,21 @@ func lengthOfLongestSubstring(s string) int {
 }
 
 func lengthOfLongestSubstringAlt(s string) int {
-	charMap := make(map[rune]int)
-	start, window := 0, 0
+	// 定义游标尺寸大小,游标的左边位置
+	window, start := 0, 0
 
-	for i, c := range s {
-		if _, ok := charMap[c]; !ok || charMap[c] < start {
-			charMap[c] = i
-			window = int(math.Max(float64(i-start+1), float64(window)))
-		} else {
-			start = charMap[c] + 1
-			charMap[c] = i
+	// 循环字符串
+	for key := 0; key < len(s); key++ {
+		// 查看当前字符串是否在游标内
+		isExist := strings.Index(string(s[start:key]), string(s[key]))
+
+		// 如果不存在游标内部,游标长度重新计算并赋值
+		if isExist == -1 {
+			if key-start+1 > window {
+				window = key - start + 1
+			}
+		} else { //存在，游标开始位置更换为重复字符串位置的下一个位置
+			start = start + 1 + isExist
 		}
 	}
 	return window
