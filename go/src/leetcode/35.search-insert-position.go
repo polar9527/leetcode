060ff2ffc0e3599a84bcf96abe55ac7b82fa9653
polar9527 <polar9527@163.com>
2020-03-14 01:53:27 +0800

    go:35

---
+++ go/src/leetcode/35.search-insert-position.go
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=35 lang=golang
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (41.47%)
+ * Likes:    1871
+ * Dislikes: 225
+ * Total Accepted:    530.5K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ *
+ * You may assume no duplicates in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ *
+ *
+ */
+package main
+
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if target <= nums[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
+
+// @lc code=end
