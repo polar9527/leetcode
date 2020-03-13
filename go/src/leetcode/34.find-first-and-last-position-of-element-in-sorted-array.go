d975dc1c74e78e86603573eb24e0afa9df9fd520
polar9527 <polar9527@163.com>
2020-03-14 01:46:10 +0800

    go:34

---
+++ go/src/leetcode/34.find-first-and-last-position-of-element-in-sorted-array.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=34 lang=golang
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (35.08%)
+ * Likes:    2701
+ * Dislikes: 122
+ * Total Accepted:    431.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * If the target is not found in the array, return [-1, -1].
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ *
+ */
+package main
+
+// @lc code=start
+func searchRange(nums []int, target int) []int {
+	ret := []int{-1, -1}
+	// lower
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if target == nums[mid] {
+			ret[0] = mid
+			r = mid - 1
+		} else if target < nums[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	// upper
+	l, r = 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if target == nums[mid] {
+			ret[1] = mid
+			l = mid + 1
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ret
+}
+
+// @lc code=end
