3c335d12d17bcd26bf589137066ed11fa5cafa0b
polar9527 <polar9527@163.com>
2020-03-19 15:44:28 +0800

    offer:6

---
+++ go/src/offer/6.reversePrint.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type List struct {
+	head *ListNode
+	tail *ListNode
+}
+
+func (l *List) addNode(val int) {
+	newNode := &ListNode{val, nil}
+	if l.head == nil {
+		l.head = newNode
+	}
+	if l.tail == nil {
+		l.tail = newNode
+		return
+	}
+	l.tail.Next = newNode
+	l.tail = newNode
+}
+
+func main() {
+	l := new(List)
+	l.addNode(1)
+	l.addNode(2)
+	l.addNode(3)
+	l.addNode(4)
+	l.addNode(5)
+	l.addNode(6)
+
+	s := reversePrint(l.head)
+	fmt.Println(s)
+}
+
+func reversePrint(head *ListNode) []int {
+	cur := head
+	var stack []int
+	if head == nil {
+		return stack
+	}
+	for cur != nil {
+		stack = append(stack, cur.Val)
+		cur = cur.Next
+	}
+
+	for lo, hi := 0, len(stack)-1; lo < hi; {
+		stack[lo], stack[hi] = stack[hi], stack[lo]
+		lo++
+		hi--
+	}
+	return stack
+}
