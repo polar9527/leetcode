9d1ffee4ac1fd009b1b9de56056fcb67e70a4291
polar9527 <polar9527@163.com>
2020-03-20 18:06:33 +0800

    offer:10.1 && go:509

--- go/src/offer/10.1.fibonacci.go
+++ go/src/offer/10.1.fibonacci.go
@@ -2,21 +2,9 @@ package main
 
 func fib(n int) int {
 
-	if n < 0 {
-		return -1
+	a, b := 0, 1
+	for i := 0; i < n; i++ {
+		a, b = a+b, a
 	}
-	result := []int{0, 1}
-	if n < 2 {
-		return result[n]
-	}
-
-	fibN := 0
-	fib1 := 0
-	fib2 := 1
-	for i := 2; i <= n; i++ {
-		fibN = fib2 + fib1
-		fib1 = fib2
-		fib2 = fibN
-	}
-	return fibN
+	return a
 }
