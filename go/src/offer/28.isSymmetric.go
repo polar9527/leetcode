75297bda4072cd9cf6eb77da474eca1375d9ffd5
polar9527 <polar9527@163.com>
2020-04-06 10:52:20 +0800

    offer:28

---
+++ go/src/offer/28.isSymmetric.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSymmetric(root *TreeNode) bool {
+	return isSymmetricCore(root, root)
+}
+
+func isSymmetricCore(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isSymmetricCore(left.Left, right.Right) && isSymmetricCore(left.Right, right.Left)
+}
