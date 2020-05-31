3b38b3360d5ba643c8d147e9481b9576973efa0d
polar <polar9527@163.com>
2020-05-31 20:12:18 +0800

    go:101 - 对称二叉树

---
+++ go/src/leetcode/101.对称二叉树.go
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=golang
+ *
+ * [101] 对称二叉树
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (50.89%)
+ * Likes:    780
+ * Dislikes: 0
+ * Total Accepted:    140.2K
+ * Total Submissions: 274K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ *
+ *
+ *
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你可以运用递归和迭代两种方法解决这个问题吗？
+ *
+ */
+package main
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isMirror(root.Left, root.Right)
+}
+
+func isMirror(l, r *TreeNode) bool {
+	if l == nil && r == nil {
+		return true
+	}
+	if l != nil && r != nil {
+		if l.Val == r.Val {
+			return isMirror(l.Left, r.Right) && isMirror(l.Right, r.Left)
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+
+// @lc code=end
