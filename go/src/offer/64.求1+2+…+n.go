8237df3803669d2545d0e748d1b09a4d5588b690
polar <polar9527@163.com>
2020-06-02 08:39:11 +0800

    offer:64

--- go/src/offer/64.求1+2+…+n.go
+++ go/src/offer/64.求1+2+…+n.go
@@ -1,9 +1,14 @@
 package main
 
 func sumNums(n int) int {
-	if n == 1 {
-		return 1
+	ans := 0
+
+	var sum func(int) bool
+	sum = func(n int) bool {
+		ans += n
+		return n > 0 && sum(n-1)
 	}
 
-	return n + sumNums(n-1)
+	sum(n)
+	return ans
 }
