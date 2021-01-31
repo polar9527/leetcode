2934a2d5660cab655232a50c67e6781c9c5d712e
polar <polar9527@163.com>
2021-01-31 12:33:50 +0800

    optimize bubbule sort

--- go/src/sort/bubbleSort.go
+++ go/src/sort/bubbleSort.go
@@ -7,11 +7,17 @@ func bubbleSort(arr []int) {
 		return
 	}
 
+	var swapped bool
 	for i := 0; i < l-1; i++ {
+		swapped = false
 		for j := l - 1; j > 0; j-- {
 			if arr[j] < arr[j-1] {
 				arr[j], arr[j-1] = arr[j-1], arr[j]
+				swapped = true
 			}
+		}
+		if !swapped {
+			break
 		}
 	}
 }
--- go/src/sort/bubbleSort_test.go
+++ go/src/sort/bubbleSort_test.go
@@ -8,4 +8,8 @@ func TestBubbleSort(t *testing.T) {
 	array := []int{3, 2, 7, 4, 9, 7, 8, 6, 7, 11, 0, 4, 5}
 	bubbleSort(array)
 	t.Log(array)
+
+	array1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 99}
+	bubbleSort(array1)
+	t.Log(array1)
 }
