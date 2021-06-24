55d4b9a071ceed12c5ecbaaead8b76a37a36c596
polar <polar9527@163.com>
2021-06-24 23:05:44 +0800

    go:79.单词搜索

---
+++ go/src/offer/79.单词搜索.go
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode.cn id=79 lang=golang
+ *
+ * [79] 单词搜索
+ *
+ * https://leetcode-cn.com/problems/word-search/description/
+ *
+ * algorithms
+ * Medium (45.19%)
+ * Likes:    936
+ * Dislikes: 0
+ * Total Accepted:    184K
+ * Total Submissions: 407.2K
+ * Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+ *
+ * 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false
+ * 。
+ *
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word =
+ * "ABCCED"
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word =
+ * "SEE"
+ * 输出：true
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word =
+ * "ABCB"
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == board.length
+ * n = board[i].length
+ * 1
+ * 1
+ * board 和 word 仅由大小写英文字母组成
+ *
+ *
+ *
+ *
+ * 进阶：你可以使用搜索剪枝的技术来优化解决方案，使其在 board 更大的情况下可以更快解决问题？
+ *
+ */
+
+// @lc code=start
+
+type pair struct{ x, y int }
+
+var direction = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 上下左右
+
+func exist(board [][]byte, word string) bool {
+	h, w := len(board), len(board[0])
+	visit := make([][]bool, h)
+	for i := range visit {
+		visit[i] = make([]bool, w)
+	}
+
+	var check func(i, j, k int) bool
+	check = func(i, j, k int) bool {
+		if board[i][j] != word[k] {
+			return false
+		}
+		if k == len(word)-1 {
+			return true
+		}
+
+		visit[i][j] = true
+		defer func() { visit[i][j] = false }()
+		for _, d := range direction {
+			newI, newJ := i+d.x, j+d.y
+			if 0 <= newI && newI < h && 0 <= newJ && newJ < w && !visit[newI][newJ] {
+				if check(newI, newJ, k+1) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
+	for i, row := range board {
+		for j := range row {
+			if check(i, j, 0) {
+				return true
+			}
+		}
+	}
+}
+
+// @lc code=end
+
