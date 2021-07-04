6aab29ad52f4f0226a176bc79be6f8094bced857
polar <polar9527@163.com>
2021-07-04 22:46:22 +0800

    go:136.只出现一次的数字 update

--- go/src/leetcode/136.只出现一次的数字.go
+++ go/src/leetcode/136.只出现一次的数字.go
@@ -2,16 +2,44 @@
  * @lc app=leetcode.cn id=136 lang=golang
  *
  * [136] 只出现一次的数字
+ *
+ * https://leetcode-cn.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (71.60%)
+ * Likes:    1910
+ * Dislikes: 0
+ * Total Accepted:    430.5K
+ * Total Submissions: 600.9K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
  */
 package leetcode
 
 // @lc code=start
 func singleNumber(nums []int) int {
-	ret := 0
-	for _, v := range nums {
-		ret ^= v
+	var res int
+	for _, num := range nums {
+		res = num ^ res
 	}
-	return ret
+	return res
 }
 
 // @lc code=end
