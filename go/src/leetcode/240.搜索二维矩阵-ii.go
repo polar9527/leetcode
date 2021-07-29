c0e7186c68b0145f5b878ebd72003d712d83e5f8
Sun Hui <huix.sun@intel.com>
2021-07-29 18:05:05 -0400

    go:240.搜索二维矩阵-ii update

--- go/src/leetcode/240.搜索二维矩阵-ii.go
+++ go/src/leetcode/240.搜索二维矩阵-ii.go
@@ -2,107 +2,118 @@
  * @lc app=leetcode.cn id=240 lang=golang
  *
  * [240] 搜索二维矩阵 II
+ *
+ * https://leetcode-cn.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (46.75%)
+ * Likes:    676
+ * Dislikes: 0
+ * Total Accepted:    148.7K
+ * Total Submissions: 317.1K
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n' +
+  '5'
+ *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * 
+ * 
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 5
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 20
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 
+ * -10^9 
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -10^9 
+ * 
+ * 
  */
-package leetcode
-
-import "fmt"
 
 // @lc code=start
 func searchMatrix(matrix [][]int, target int) bool {
-	// matrix[rowIndex][columnIndex]
-	var rowLength, columnLength int
-	rowLength = len(matrix)
-	if rowLength == 0 {
-		return false
-	}
+    
+    rows := len(matrix)
+    if rows == 0 {
+        return false
+    }
+    columns := len(matrix[0])
 
-	columnLength = len(matrix[0])
-	if 0 == columnLength {
-		return false
-	}
+    if rows == 1 && columns == 1 {
+        return matrix[0][0] == target
+    }
 
-	// if rowLength == 1 {
-	// 	location := sort.SearchInts(matrix[0], target)
-	// 	return location < columnLength && matrix[0][location] == target
-	// }
+    if rows == 1 {
+        
+        l, h := 0, columns-1
+        for l <= h {
+            mid := (l+h)/2
+            if matrix[0][mid] == target {
+                return true
+            } else if matrix[0][mid] < target {
+                l = mid + 1
+            } else {
+                h = mid - 1
+            }
+        }
+        return false
+    }
 
-	// if columnLength == 1 {
-	// 	location := sort.Search(rowLength, func(i int) {
-	// 		return matrix[i][0] >= target
-	// 	})
-	// 	return location < rowLength && matrix[location][0] == target
-	// }
-	if rowLength == 1 {
-		if target < matrix[0][0] || matrix[0][columnLength-1] < target {
-			return false
-		} else {
-			if ret := binSearch(matrix[0], target); ret >= 0 {
-				return true
-			} else {
-				fmt.Printf("location: %d\n", ret)
-				return false
-			}
-		}
-	}
+    if columns == 1 {
+        l, h := 0, rows-1
+        for l <= h {
+            mid := (l+h)/2
+            if matrix[mid][0] == target {
+                return true
+            } else if matrix[mid][0] < target {
+                l = mid + 1
+            } else {
+                h = mid - 1
+            }
+        }
+        return false
+    }
 
-	if columnLength == 1 {
-		if target < matrix[0][0] || matrix[rowLength-1][0] < target {
-			return false
-		} else {
-			array := make([]int, rowLength)
-			for i := range matrix {
-				array[i] = matrix[i][0]
-			}
-			if ret := binSearch(array, target); ret >= 0 {
-				return true
-			} else {
-				return false
-			}
-		}
-	}
+    for r, c := rows - 1, 0; r >= 0 && c < columns; {
+        if matrix[r][c] == target {
+            return true
+        } else if target > matrix[r][c] {
+            c++
+            continue
+        } else {
+            r--
+            continue
+        }
+    } 
 
-	if target < matrix[0][0] || matrix[rowLength-1][columnLength-1] < target {
-		return false
-	}
-
-	for r, c := rowLength-1, 0; r >= 0 && c < columnLength; {
-		if matrix[r][c] < target {
-			c++
-		} else if matrix[r][c] > target {
-			r--
-		} else {
-			return true
-		}
-	}
-	return false
+    return false
 }
-
-func binSearch(a []int, target int) int {
-	if len(a) == 0 {
-		return -1
-	}
-	if len(a) == 1 {
-		if a[0] == target {
-			return 0
-		}
-		return -1
-	}
-	lo, hi, mi := 0, len(a)-1, len(a)/2
-	for ; lo < hi && a[mi] != target; mi = lo + (hi-lo)/2 {
-
-		if a[mi] == target {
-			return mi
-		} else if a[mi] < target {
-			lo = mi + 1
-		} else {
-			hi = mi - 1
-		}
-	}
-	if a[mi] == target {
-		return mi
-	} else {
-		return -1
-	}
-}
-
 // @lc code=end
+
