ea3fb72803a28dacf0a0c2004db885e088ccc881
polar <polar9527@163.com>
2023-06-29 10:29:14 +0800

    recover demo code

---
+++ leetcode-go/demo/defer-demo/recover/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	defer fmt.Println("defer main")
+	var user = os.Getenv("USER_")
+
+	go func() {
+		defer func() {
+			fmt.Println("defer caller")
+			if err := recover(); err != nil {
+				fmt.Println("recover success. err:", err)
+			}
+		}()
+
+		func() {
+			defer func() {
+				fmt.Println("defer here")
+			}()
+			if user == "" {
+				panic("should set user env.")
+			}
+
+			// 此处不会执行
+			fmt.Println("after panic")
+		}()
+	}()
+
+	time.Sleep(100)
+	fmt.Println("end of main function")
+}
