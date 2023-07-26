31b4ae4e786c8c9d163ee417cc7a9990f2e55f8a
polar <polar9527@163.com>
2023-07-26 11:06:39 +0800

    offer II 007.func three-sum

---
+++ leetcode-go/offer II/007.func three-sum/007.func three-sum_test.go
@@ -0,0 +1,26 @@
+package offer2
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"sample", args{int[1.-1.-1,0]}, [][]int{}}
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
