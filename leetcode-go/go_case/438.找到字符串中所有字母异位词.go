055ae9ab5ac187195b015790e3234a88794f67d1
polar <polar9527@163.com>
2023-07-31 22:54:00 +0800

    offer 2 015.find-all-anagrams-in-a-string

---
+++ leetcode-go/go_case/438.找到字符串中所有字母异位词.go
@@ -0,0 +1,82 @@
+package go_case
+
+/*
+ * @lc app=leetcode.cn id=438 lang=golang
+ *
+ * [438] 找到字符串中所有字母异位词
+ *
+ * https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/
+ *
+ * algorithms
+ * Medium (54.62%)
+ * Likes:    1233
+ * Dislikes: 0
+ * Total Accepted:    304K
+ * Total Submissions: 556.8K
+ * Testcase Example:  '"cbaebabacd"\n"abc"'
+ *
+ * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+ *
+ * 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: s = "cbaebabacd", p = "abc"
+ * 输出: [0,6]
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: s = "abab", p = "ab"
+ * 输出: [0,1,2]
+ * 解释:
+ * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+ * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+ * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= s.length, p.length <= 3 * 10^4
+ * s 和 p 仅包含小写字母
+ *
+ *
+ */
+
+// @lc code=start
+func findAnagrams(s, p string) (ans []int) {
+	sLen, pLen := len(s), len(p)
+	if sLen < pLen {
+		return
+	}
+
+	var sCount, pCount [26]int
+	for i, ch := range p {
+		sCount[s[i]-'a']++
+		pCount[ch-'a']++
+	}
+	if sCount == pCount {
+		ans = append(ans, 0)
+	}
+
+	for i, ch := range s[:sLen-pLen] {
+		sCount[ch-'a']--
+		sCount[s[i+pLen]-'a']++
+		if sCount == pCount {
+			ans = append(ans, i+1)
+		}
+	}
+	return
+}
+
+// @lc code=end
