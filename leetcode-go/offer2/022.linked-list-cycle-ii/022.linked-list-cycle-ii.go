3ebfcda442002f1f49975caf6bf9183f2b37e65f
polar <polar9527@163.com>
2023-08-03 00:12:41 +0800

    offer2 022.linked-list-cycle-ii

---
+++ leetcode-go/offer2/022.linked-list-cycle-ii/022.linked-list-cycle-ii.go
@@ -0,0 +1,111 @@
+package offer2
+
+/*
+ * @lc app=leetcode.cn id=142 lang=golang
+ *
+ * [142] 环形链表 II
+ *
+ * https://leetcode.cn/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (57.20%)
+ * Likes:    2241
+ * Dislikes: 0
+ * Total Accepted:    715.2K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos
+ * 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos
+ * 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 不允许修改 链表。
+ *
+ *
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：返回索引为 0 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ *
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：返回 null
+ * 解释：链表中没有环。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目范围在范围 [0, 10^4] 内
+ * -10^5 <= Node.val <= 10^5
+ * pos 的值为 -1 或者链表中的一个有效索引
+ *
+ *
+ *
+ *
+ * 进阶：你是否可以使用 O(1) 空间解决此题？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	// fast 进入圆圈之后，即便是在最坏情况下,即slow刚进入圆圈的那一刻，fast正好在slow前面一丁点，slow 在圆圈之中走完第一圈之前，fast必定能遇上slow
+	// 基于这个前提,
+	// 设链表中环外部分的长度为 a。slow指针进入环后，又走了 b 的距离与 fast 相遇。
+	// 此时，fast 指针已经走完了环的 n 圈，因此它走过的总距离为 a+n(b+c)+b=a+(n+1)b+nc。
+	// 根据题意，任意时刻，fast 指针走过的距离都为 slow 指针的 2 倍。
+	// 因此，我们有a(n+1)b+nc=2(a+b)⟹a=c+(n−1)(b+c)
+	slow, fast := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
+
+// @lc code=end
