71706c1918df02c9b08f2c60bf60c7fa4b933c81
polar <polar9527@163.com>
2023-08-03 01:03:30 +0800

    fix

--- leetcode-go/go_case/160.相交链表.go
+++ leetcode-go/go_case/160.相交链表.go
@@ -115,7 +115,23 @@ package go_case
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-
+	if headA == nil || headB == nil {
+		return nil
+	}
+	pa, pb := headA, headB
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+	return pa
 }
 
 // @lc code=end
