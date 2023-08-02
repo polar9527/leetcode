1688d16761757ca6501f7a66eb01e4d7405cd634
polar <polar9527@163.com>
2023-08-03 00:53:28 +0800

    fix

--- leetcode-go/go_case/19.删除链表的倒数第-n-个结点.go
+++ leetcode-go/go_case/19.删除链表的倒数第-n-个结点.go
@@ -69,6 +69,9 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	dummy := &ListNode{0, head}
 	first, second := head, dummy
 	for i := 0; i < n; i++ {
+		if first.Next == nil {
+			return first.Next
+		}
 		first = first.Next
 	}
 	for ; first != nil; first = first.Next {
