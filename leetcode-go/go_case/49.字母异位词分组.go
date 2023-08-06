4442c5f1a3e11a150cafcfc5112245a45ab86867
polar <polar9527@163.com>
2023-08-06 21:45:28 +0800

    offer 2 033.group-anagrams

---
+++ leetcode-go/go_case/49.字母异位词分组.go
@@ -0,0 +1,73 @@
+package go_case
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=49 lang=golang
+ *
+ * [49] 字母异位词分组
+ *
+ * https://leetcode.cn/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (67.83%)
+ * Likes:    1552
+ * Dislikes: 0
+ * Total Accepted:    493K
+ * Total Submissions: 726.7K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ *
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ *
+ *
+ */
+
+// @lc code=start
+func groupAnagrams(strs []string) [][]string {
+	m := map[string][]string{}
+	for _, str := range strs {
+		s := []byte(str)
+		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+		sortedStr := string(s)
+		m[sortedStr] = append(m[sortedStr], str)
+	}
+	ans := make([][]string, 0, len(m))
+	for _, v := range m {
+		ans = append(ans, v)
+	}
+	return ans
+}
+
+// @lc code=end
