df6dbff84d323ea3254d94deb306163be7fe5970
polar <polar9527@163.com>
2023-08-15 08:56:45 +0800

    offer 2 048.serialize-and-deserialize-binary-tree

---
+++ leetcode-go/go_case/297.二叉树的序列化与反序列化.go
@@ -0,0 +1,128 @@
+package go_case
+
+import (
+	"strconv"
+	"strings"
+)
+
+/*
+ * @lc app=leetcode.cn id=297 lang=golang
+ *
+ * [297] 二叉树的序列化与反序列化
+ *
+ * https://leetcode.cn/problems/serialize-and-deserialize-binary-tree/description/
+ *
+ * algorithms
+ * Hard (58.90%)
+ * Likes:    1140
+ * Dislikes: 0
+ * Total Accepted:    221K
+ * Total Submissions: 375.1K
+ * Testcase Example:  '[1,2,3,null,null,4,5]'
+ *
+ *
+ * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+ *
+ * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 /
+ * 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ *
+ * 提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode
+ * 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,null,null,4,5]
+ * 输出：[1,2,3,null,null,4,5]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中结点数在范围 [0, 10^4] 内
+ * -1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type Codec struct{}
+
+func Constructor() (_ Codec) {
+	return
+}
+
+func (Codec) serialize(root *TreeNode) string {
+	sb := &strings.Builder{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			sb.WriteString("null,")
+			return
+		}
+		sb.WriteString(strconv.Itoa(node.Val))
+		sb.WriteByte(',')
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return sb.String()
+}
+
+func (Codec) deserialize(data string) *TreeNode {
+	sp := strings.Split(data, ",")
+	var build func() *TreeNode
+	build = func() *TreeNode {
+		if sp[0] == "null" {
+			sp = sp[1:]
+			return nil
+		}
+		val, _ := strconv.Atoi(sp[0])
+		sp = sp[1:]
+		return &TreeNode{val, build(), build()}
+	}
+	return build()
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * ser := Constructor();
+ * deser := Constructor();
+ * data := ser.serialize(root);
+ * ans := deser.deserialize(data);
+ */
+// @lc code=end
