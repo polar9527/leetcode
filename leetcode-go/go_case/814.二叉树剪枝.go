943b7398acad4965eba2d566b24b32653392a1fc
polar <polar9527@163.com>
2023-08-15 08:47:40 +0800

    offer 2 047.binary-tree-pruning

---
+++ leetcode-go/go_case/814.二叉树剪枝.go
@@ -0,0 +1,97 @@
+package go_case
+
+/*
+ * @lc app=leetcode.cn id=814 lang=golang
+ *
+ * [814] 二叉树剪枝
+ *
+ * https://leetcode.cn/problems/binary-tree-pruning/description/
+ *
+ * algorithms
+ * Medium (72.53%)
+ * Likes:    342
+ * Dislikes: 0
+ * Total Accepted:    59.9K
+ * Total Submissions: 82.6K
+ * Testcase Example:  '[1,null,0,0,1]'
+ *
+ * 给你二叉树的根结点 root ，此外树的每个结点的值要么是 0 ，要么是 1 。
+ *
+ * 返回移除了所有不包含 1 的子树的原二叉树。
+ *
+ * 节点 node 的子树为 node 本身加上所有 node 的后代。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,0,0,1]
+ * 输出：[1,null,0,null,1]
+ * 解释：
+ * 只有红色节点满足条件“所有不包含 1 的子树”。 右图为返回的答案。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,0,1,0,0,0,1]
+ * 输出：[1,null,1,null,1]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,1,0,1,1,0,1,0]
+ * 输出：[1,1,0,1,1,null,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目在范围 [1, 200] 内
+ * Node.val 为 0 或 1
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pruneTree(root *TreeNode) *TreeNode {
+	var dfs func(node *TreeNode) bool
+	// 只要包含 1， 就返回false
+	dfs = func(node *TreeNode) bool {
+		if node == nil {
+			return true
+		}
+
+		ans := false
+		removeLeft := dfs(node.Left)
+		removeRight := dfs(node.Right)
+		if removeLeft {
+			node.Left = nil
+		}
+		if removeRight {
+			node.Right = nil
+		}
+		if node.Val == 0 {
+			ans = removeLeft && removeRight
+		}
+		return ans
+	}
+	if dfs(root) {
+		return nil
+	}
+	return root
+}
+
+// @lc code=end
