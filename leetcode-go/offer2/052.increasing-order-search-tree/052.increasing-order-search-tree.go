9b3af669e0eba76a17510e4e5492a487131bc89c
polar <polar9527@163.com>
2023-08-16 13:33:31 +0800

    offer 2 052.increasing-order-search-tree

---
+++ leetcode-go/offer2/052.increasing-order-search-tree/052.increasing-order-search-tree.go
@@ -0,0 +1,78 @@
+package offer2
+
+/*
+ * @lc app=leetcode.cn id=897 lang=golang
+ *
+ * [897] 递增顺序搜索树
+ *
+ * https://leetcode.cn/problems/increasing-order-search-tree/description/
+ *
+ * algorithms
+ * Easy (74.00%)
+ * Likes:    329
+ * Dislikes: 0
+ * Total Accepted:    79.1K
+ * Total Submissions: 106.9K
+ * Testcase Example:  '[5,3,6,2,4,null,8,1,null,null,null,7,9]'
+ *
+ * 给你一棵二叉搜索树的 root ，请你 按中序遍历
+ * 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,1,7]
+ * 输出：[1,null,5,null,7]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数的取值范围是 [1, 100]
+ * 0 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func increasingBST(root *TreeNode) *TreeNode {
+	vals := []int{}
+	var inorder func(*TreeNode)
+	inorder = func(node *TreeNode) {
+		if node != nil {
+			inorder(node.Left)
+			vals = append(vals, node.Val)
+			inorder(node.Right)
+		}
+	}
+	inorder(root)
+
+	dummyNode := &TreeNode{}
+	curNode := dummyNode
+	for _, val := range vals {
+		curNode.Right = &TreeNode{Val: val}
+		curNode = curNode.Right
+	}
+	return dummyNode.Right
+}
+
+// @lc code=end
