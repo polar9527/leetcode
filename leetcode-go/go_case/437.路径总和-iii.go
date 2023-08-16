a064297f20f4b8d40f6e608c52846564063f5d0d
polar <polar9527@163.com>
2023-08-16 13:08:45 +0800

    offer 2 050.path-sum-iii

---
+++ leetcode-go/go_case/437.路径总和-iii.go
@@ -0,0 +1,109 @@
+package go_case
+
+/*
+ * @lc app=leetcode.cn id=437 lang=golang
+ *
+ * [437] 路径总和 III
+ *
+ * https://leetcode.cn/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Medium (49.85%)
+ * Likes:    1682
+ * Dislikes: 0
+ * Total Accepted:    240.1K
+ * Total Submissions: 482.9K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ *
+ * 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * 输出：3
+ * 解释：和等于 8 的路径有 3 条，如图所示。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：3
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 二叉树的节点个数的范围是 [0,1000]
+ * -10^9
+ * -1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+//  DFS
+func rootSum(root *TreeNode, targetSum int) (res int) {
+	if root == nil {
+		return
+	}
+	val := root.Val
+	if val == targetSum {
+		res++
+	}
+	res += rootSum(root.Left, targetSum-val)
+	res += rootSum(root.Right, targetSum-val)
+	return
+}
+
+func pathSum(root *TreeNode, targetSum int) int {
+	if root == nil {
+		return 0
+	}
+	res := rootSum(root, targetSum)
+	res += pathSum(root.Left, targetSum)
+	res += pathSum(root.Right, targetSum)
+	return res
+}
+
+// func rootSum(root *TreeNode, targetSum int) (res int) {
+//     if root == nil {
+//         return
+//     }
+//     val := root.Val
+//     if val == targetSum {
+//         res++
+//     }
+//     res += rootSum(root.Left, targetSum-val)
+//     res += rootSum(root.Right, targetSum-val)
+//     return
+// }
+
+// func pathSum(root *TreeNode, targetSum int) int {
+//     if root == nil {
+//         return 0
+//     }
+//     res := rootSum(root, targetSum)
+//     res += pathSum(root.Left, targetSum)
+//     res += pathSum(root.Right, targetSum)
+//     return res
+// }
+
+// @lc code=end
