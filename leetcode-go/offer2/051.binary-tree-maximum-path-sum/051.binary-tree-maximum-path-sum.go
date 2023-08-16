d5171527925c3311a1b3316acd15e74965cf68dd
polar <polar9527@163.com>
2023-08-16 17:39:27 +0800

    fix

---
+++ leetcode-go/offer2/051.binary-tree-maximum-path-sum/051.binary-tree-maximum-path-sum.go
@@ -0,0 +1,97 @@
+package offer2
+
+import "math"
+
+/*
+ * @lc app=leetcode.cn id=124 lang=golang
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (45.28%)
+ * Likes:    2026
+ * Dislikes: 0
+ * Total Accepted:    337.9K
+ * Total Submissions: 745.9K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ *
+ * 路径和 是路径中各节点值的总和。
+ *
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxPathSum(root *TreeNode) int {
+	maxSum := math.MinInt32
+	var maxGain func(*TreeNode) int
+	maxGain = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+
+		// 递归计算左右子节点的最大贡献值
+		// 只有在最大贡献值大于 0 时，才会选取对应子节点
+		leftGain := max(maxGain(node.Left), 0)
+		rightGain := max(maxGain(node.Right), 0)
+
+		// 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+		priceNewPath := node.Val + leftGain + rightGain
+
+		// 更新答案
+		maxSum = max(maxSum, priceNewPath)
+
+		// 返回节点的最大贡献值
+		return node.Val + max(leftGain, rightGain)
+	}
+	maxGain(root)
+	return maxSum
+}
+
+// func max(x, y int) int {
+// 	if x > y {
+// 		return x
+// 	}
+// 	return y
+// }
+
+// @lc code=end
