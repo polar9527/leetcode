e8a3ad5a88d01ca9929ea066b7f32b56892b5d6a
polar <polar9527@163.com>
2023-08-21 11:32:21 +0800

    offer 2 062.implement-trie-prefix-tree

---
+++ leetcode-go/go_case/208.实现-trie-前缀树.go
@@ -0,0 +1,116 @@
+package go_case
+
+/*
+ * @lc app=leetcode.cn id=208 lang=golang
+ *
+ * [208] 实现 Trie (前缀树)
+ *
+ * https://leetcode.cn/problems/implement-trie-prefix-tree/description/
+ *
+ * algorithms
+ * Medium (71.92%)
+ * Likes:    1513
+ * Dislikes: 0
+ * Total Accepted:    277.2K
+ * Total Submissions: 385.4K
+ * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
+  '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+ *
+ * Trie（发音类似 "try"）或者说 前缀树
+ * 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+ *
+ * 请你实现 Trie 类：
+ *
+ *
+ * Trie() 初始化前缀树对象。
+ * void insert(String word) 向前缀树中插入字符串 word 。
+ * boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回
+ * false 。
+ * boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true
+ * ；否则，返回 false 。
+ *
+ *
+ *
+ *
+ * 示例：
+ *
+ *
+ * 输入
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * 输出
+ * [null, null, true, false, true, null, true]
+ *
+ * 解释
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 True
+ * trie.search("app");     // 返回 False
+ * trie.startsWith("app"); // 返回 True
+ * trie.insert("app");
+ * trie.search("app");     // 返回 True
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * word 和 prefix 仅由小写英文字母组成
+ * insert、search 和 startsWith 调用次数 总计 不超过 3 * 10^4 次
+ *
+ *
+*/
+
+// @lc code=start
+type Trie struct {
+	children [26]*Trie
+	isEnd    bool
+}
+
+func Constructor() Trie {
+	return Trie{}
+}
+
+func (this *Trie) Insert(word string) {
+	node := this
+	for _, c := range word {
+		c -= 'a'
+		if node.children[c] == nil {
+			node.children[c] = &Trie{}
+		}
+		node = node.children[c]
+	}
+	node.isEnd = true
+}
+
+func (this *Trie) Search(word string) bool {
+	node := this.SearchPrefix(word)
+	return node != nil && node.isEnd
+}
+
+func (this *Trie) SearchPrefix(word string) *Trie {
+	node := this
+	for _, c := range word {
+		c -= 'a'
+		if node.children[c] == nil {
+			return nil
+		}
+		node = node.children[c]
+	}
+	return node
+}
+
+func (this *Trie) StartsWith(prefix string) bool {
+	return this.SearchPrefix(prefix) != nil
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
+// @lc code=end
