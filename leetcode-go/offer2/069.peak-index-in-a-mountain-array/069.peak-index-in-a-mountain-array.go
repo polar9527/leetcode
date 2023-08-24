6ba04d093ebfff00a7a2c0f605ea1d5eb620bd67
polar <polar9527@163.com>
2023-08-24 19:18:23 +0800

    offer 2 069.peak-index-in-a-mountain-array

---
+++ leetcode-go/offer2/069.peak-index-in-a-mountain-array/069.peak-index-in-a-mountain-array.go
@@ -0,0 +1,102 @@
+package offer2
+
+import "math"
+
+/*
+ * @lc app=leetcode.cn id=852 lang=golang
+ *
+ * [852] 山脉数组的峰顶索引
+ *
+ * https://leetcode.cn/problems/peak-index-in-a-mountain-array/description/
+ *
+ * algorithms
+ * Medium (68.58%)
+ * Likes:    365
+ * Dislikes: 0
+ * Total Accepted:    142.2K
+ * Total Submissions: 207.4K
+ * Testcase Example:  '[0,1,0]'
+ *
+ * 符合下列属性的数组 arr 称为 山脉数组 ：
+ *
+ * arr.length >= 3
+ * 存在 i（0 < i < arr.length - 1）使得：
+ *
+ * arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ *
+ *
+ *
+ *
+ * 给你由整数组成的山脉数组 arr ，返回满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i +
+ * 1] > ... > arr[arr.length - 1] 的下标 i 。
+ *
+ * 你必须设计并实现时间复杂度为 O(log(n)) 的解决方案。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：arr = [0,1,0]
+ * 输出：1
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：arr = [0,2,1,0]
+ * 输出：1
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：arr = [0,10,5,2]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 3 <= arr.length <= 10^5
+ * 0 <= arr[i] <= 10^6
+ * 题目数据保证 arr 是一个山脉数组
+ *
+ *
+ */
+
+// @lc code=start
+// func peakIndexInMountainArray(arr []int) int {
+// 	return sort.Search(len(arr)-1, func(i int) bool { return arr[i] > arr[i+1] })
+// }
+
+func peakIndexInMountainArray(nums []int) int {
+	n := len(nums)
+
+	// 辅助函数，输入下标 i，返回 nums[i] 的值
+	// 方便处理 nums[-1] 以及 nums[n] 的边界情况
+	get := func(i int) int {
+		if i == -1 || i == n {
+			return math.MinInt64
+		}
+		return nums[i]
+	}
+
+	left, right := 0, n-1
+	for {
+		mid := (left + right) / 2
+		if get(mid-1) < get(mid) && get(mid) > get(mid+1) {
+			return mid
+		}
+		if get(mid) < get(mid+1) {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+}
+
+// @lc code=end
