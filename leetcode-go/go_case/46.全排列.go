e7299181870df094f4d45089b983f9dc8c685f91
polar <polar9527@163.com>
2024-03-23 22:28:34 +0800

    offer 2 083.permute

---
+++ leetcode-go/go_case/46.全排列.go
@@ -0,0 +1,84 @@
+package go_case
+
+/*
+ * @lc app=leetcode.cn id=46 lang=golang
+ *
+ * [46] 全排列
+ *
+ * https://leetcode.cn/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (79.10%)
+ * Likes:    2847
+ * Dislikes: 0
+ * Total Accepted:    1M
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ *
+ *
+ */
+
+// @lc code=start
+func permute(nums []int) [][]int {
+	var res = [][]int{}
+	var path []int
+
+	used := make([]bool, len(nums))
+
+	var backtrack func([]int)
+	backtrack = func(nums []int) {
+		if len(path) == len(nums) {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			res = append(res, temp)
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if !used[i] {
+				used[i] = true
+				path = append(path, nums[i])
+				backtrack(nums)
+				path = path[:len(path)-1]
+				used[i] = false
+			}
+		}
+	}
+	backtrack(nums)
+	return res
+}
+
+// @lc code=end
