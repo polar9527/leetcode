e02bc77dcf7c9e5483f89c7692d34f617bdf9f0d
polar <polar9527@163.com>
2024-03-23 23:16:48 +0800

    offer 2 084.permuteUnique

---
+++ leetcode-go/go_case/47.全排列-ii.go
@@ -0,0 +1,86 @@
+package go_case
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=47 lang=golang
+ *
+ * [47] 全排列 II
+ *
+ * https://leetcode.cn/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (65.69%)
+ * Likes:    1555
+ * Dislikes: 0
+ * Total Accepted:    536.5K
+ * Total Submissions: 816.8K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * ⁠[1,2,1],
+ * ⁠[2,1,1]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ *
+ *
+ */
+
+// @lc code=start
+func permuteUnique(nums []int) [][]int {
+	var res [][]int
+	var path []int
+	var used = make([]bool, len(nums))
+
+	var backtracing func(int)
+	backtracing = func(start int) {
+		if start == len(nums) {
+			res = append(res, append([]int{}, path...))
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			// 同层去重
+			if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
+				continue
+			}
+			// 树枝去重
+			if !used[i] {
+				used[i] = true
+				path = append(path, nums[i])
+				backtracing(start + 1)
+				path = path[:len(path)-1]
+				used[i] = false
+			}
+
+		}
+	}
+	sort.Ints(nums)
+	backtracing(0)
+	return res
+
+}
+
+// @lc code=end
