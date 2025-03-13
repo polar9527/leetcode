9a59a84e4060881ce2a09147ec6cbeed5896ccab
polar <polar9527@163.com>
2025-03-13 21:37:08 +0800

    tree

--- leetcode-go/go_case/501.二叉搜索树中的众数.go
+++ leetcode-go/go_case/501.二叉搜索树中的众数.go
@@ -67,10 +67,41 @@ package go_case
  *     Right *TreeNode
  * }
  */
+// func findMode(root *TreeNode) []int {
+// 	res := make([]int, 0)
+// 	count := 1
+// 	max := 1
+// 	var prev *TreeNode
+// 	var travel func(node *TreeNode)
+// 	travel = func(node *TreeNode) {
+// 		if node == nil {
+// 			return
+// 		}
+// 		travel(node.Left)
+// 		if prev != nil && prev.Val == node.Val {
+// 			count++
+// 		} else {
+// 			count = 1
+// 		}
+// 		if count >= max {
+// 			if count > max && len(res) > 0 {
+// 				res = []int{node.Val}
+// 			} else {
+// 				res = append(res, node.Val)
+// 			}
+// 			max = count
+// 		}
+// 		prev = node
+// 		travel(node.Right)
+// 	}
+// 	travel(root)
+// 	return res
+// }
+
 func findMode(root *TreeNode) []int {
 	res := make([]int, 0)
-	count := 1
-	max := 1
+	count := 0
+	max := 0
 	var prev *TreeNode
 	var travel func(node *TreeNode)
 	travel = func(node *TreeNode) {
@@ -78,18 +109,20 @@ func findMode(root *TreeNode) []int {
 			return
 		}
 		travel(node.Left)
-		if prev != nil && prev.Val == node.Val {
+		if prev == nil { //第一个节点
+			count = 1
+		} else if prev.Val == node.Val {
 			count++
-		} else {
+		} else { //与前一个节点不相同
 			count = 1
 		}
-		if count >= max {
-			if count > max && len(res) > 0 {
-				res = []int{node.Val}
-			} else {
-				res = append(res, node.Val)
-			}
+
+		if count == max {
+			res = append(res, node.Val)
+		}
+		if count > max {
 			max = count
+			res = []int{node.Val}
 		}
 		prev = node
 		travel(node.Right)
