d9c65e2f7bb56c2c990e6e65e5232ffe596b1a6d
polar <polar9527@163.com>
2025-03-16 12:52:31 +0800

    tree

--- leetcode-go/go_case/450.删除二叉搜索树中的节点.go
+++ leetcode-go/go_case/450.删除二叉搜索树中的节点.go
@@ -82,44 +82,77 @@ package go_case
  *     Right *TreeNode
  * }
  */
+// func deleteNode(root *TreeNode, key int) *TreeNode {
+
+// 	// 第一种情况：没找到删除的节点，遍历到空节点直接返回了
+// 	// 找到删除的节点
+// 	// 第二种情况：左右孩子都为空（叶子节点），直接删除节点， 返回NULL为根节点
+// 	// 第三种情况：删除节点的左孩子为空，右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
+// 	// 第四种情况：删除节点的右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
+// 	// 第五种情况：左右孩子节点都不为空，则将删除节点的左子树头结点（左孩子）放到删除节点的右子树的最左面节点的左孩子上，返回删除节点右孩子为新的根节点。
+// 	if root == nil {
+// 		return nil
+// 	}
+// 	if root.Val == key {
+// 		if root.Left == nil && root.Right == nil {
+// 			return nil
+// 		}
+// 		if root.Left != nil && root.Right == nil {
+// 			return root.Left
+// 		}
+// 		if root.Left == nil && root.Right != nil {
+// 			return root.Right
+// 		}
+// 		curNode := root.Right
+// 		for curNode.Left != nil {
+// 			curNode = curNode.Left
+// 		}
+// 		curNode.Left = root.Left
+// 		return root.Right
+// 	}
+
+// 	if root.Val > key {
+// 		root.Left = deleteNode(root.Left, key)
+// 		return root
+// 	}
+// 	if root.Val < key {
+// 		root.Right = deleteNode(root.Right, key)
+// 		return root
+// 	}
+// 	return nil
+// }
+
 func deleteNode(root *TreeNode, key int) *TreeNode {
 
-	// 第一种情况：没找到删除的节点，遍历到空节点直接返回了
-	// 找到删除的节点
-	// 第二种情况：左右孩子都为空（叶子节点），直接删除节点， 返回NULL为根节点
-	// 第三种情况：删除节点的左孩子为空，右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
-	// 第四种情况：删除节点的右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
-	// 第五种情况：左右孩子节点都不为空，则将删除节点的左子树头结点（左孩子）放到删除节点的右子树的最左面节点的左孩子上，返回删除节点右孩子为新的根节点。
 	if root == nil {
 		return nil
 	}
 	if root.Val == key {
 		if root.Left == nil && root.Right == nil {
 			return nil
-		}
-		if root.Left != nil && root.Right == nil {
+		} else if root.Left != nil && root.Right == nil {
 			return root.Left
-		}
-		if root.Left == nil && root.Right != nil {
+		} else if root.Left == nil && root.Right != nil {
+			return root.Right
+		} else {
+			cur := root.Right
+			for cur.Left != nil {
+				cur = cur.Left
+			}
+			cur.Left = root.Left
 			return root.Right
 		}
-		curNode := root.Right
-		for curNode.Left != nil {
-			curNode = curNode.Left
-		}
-		curNode.Left = root.Left
-		return root.Right
+
 	}
 
 	if root.Val > key {
 		root.Left = deleteNode(root.Left, key)
-		return root
 	}
+
 	if root.Val < key {
 		root.Right = deleteNode(root.Right, key)
-		return root
 	}
-	return nil
+	return root
 }
 
 // @lc code=end
