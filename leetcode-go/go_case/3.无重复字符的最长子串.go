cc11ace1ddee0ce18c82070ab86afccd937de78e
polar <polar9527@163.com>
2025-03-17 14:06:18 +0800

    string

--- leetcode-go/go_case/3.无重复字符的最长子串.go
+++ leetcode-go/go_case/3.无重复字符的最长子串.go
@@ -56,26 +56,49 @@ package go_case
  */
 
 // @lc code=start
+// func lengthOfLongestSubstring(s string) int {
+// 	// 哈希集合，记录每个字符是否出现过
+// 	m := map[byte]int{}
+// 	n := len(s)
+// 	// 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+// 	rk, ans := -1, 0
+// 	for i := 0; i < n; i++ {
+// 		if i != 0 {
+// 			// 左指针向右移动一格，移除一个字符
+// 			delete(m, s[i-1])
+// 		}
+// 		for rk+1 < n && m[s[rk+1]] == 0 {
+// 			// 不断地移动右指针
+// 			m[s[rk+1]]++
+// 			rk++
+// 		}
+// 		// 第 i 到 rk 个字符是一个极长的无重复字符子串
+// 		ans = max(ans, rk-i+1)
+// 	}
+// 	return ans
+// }
+
 func lengthOfLongestSubstring(s string) int {
-	// 哈希集合，记录每个字符是否出现过
-	m := map[byte]int{}
+	l, r := 0, 0
 	n := len(s)
-	// 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-	rk, ans := -1, 0
-	for i := 0; i < n; i++ {
-		if i != 0 {
-			// 左指针向右移动一格，移除一个字符
-			delete(m, s[i-1])
+	var res int
+	charMap := make(map[byte]int, 0)
+	for i := l; i < n; i++ {
+		if i > 0 {
+			delete(charMap, s[i-1])
 		}
-		for rk+1 < n && m[s[rk+1]] == 0 {
-			// 不断地移动右指针
-			m[s[rk+1]]++
-			rk++
+
+		for r < n && charMap[s[r]] == 0 {
+			charMap[s[r]]++
+			r++
+
 		}
-		// 第 i 到 rk 个字符是一个极长的无重复字符子串
-		ans = max(ans, rk-i+1)
+		// 此时，s[i:r] 内的 字符都是唯一的，但是 s[r] 与 s[i:r]中的某个字符相同
+		if res < r-i {
+			res = r - i
+		}
 	}
-	return ans
+	return res
 }
 
 // @lc code=end
