ae2a47d452ddab9995210460d22df7223a05391f
polar <polar9527@163.com>
2025-04-01 15:25:13 +0800

    dp

---
+++ leetcode-go/go_case/718.最长重复子数组.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=718 lang=golang
+ *
+ * [718] 最长重复子数组
+ *
+ * https://leetcode.cn/problems/maximum-length-of-repeated-subarray/description/
+ *
+ * algorithms
+ * Medium (56.82%)
+ * Likes:    1167
+ * Dislikes: 0
+ * Total Accepted:    298.4K
+ * Total Submissions: 525.2K
+ * Testcase Example:  '[1,2,3,2,1]\n[3,2,1,4,7]'
+ *
+ * 给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+ * 输出：3
+ * 解释：长度最长的公共子数组是 [3,2,1] 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+ * 输出：5
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 100
+ *
+ *
+ */
+
+// @lc code=start
+func findLength(nums1 []int, nums2 []int) int {
+	l1 := len(nums1)
+	l2 := len(nums2)
+
+	dp := make([][]int, l1+1)
+	for i := range dp {
+		dp[i] = make([]int, l2+1)
+	}
+	res := 0
+	for i := 1; i <= l1; i++ {
+		for j := 1; j <= l2; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			}
+			if dp[i][j] > res {
+				res = dp[i][j]
+			}
+		}
+	}
+	return res
+}
+
+// @lc code=end
+
