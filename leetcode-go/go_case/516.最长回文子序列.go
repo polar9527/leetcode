30ed5def3e3a4ee7af956f54dc057bd55bfe9d53
polar <polar9527@163.com>
2025-04-03 15:56:12 +0800

    dp

---
+++ leetcode-go/go_case/516.最长回文子序列.go
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=golang
+ *
+ * [516] 最长回文子序列
+ *
+ * https://leetcode.cn/problems/longest-palindromic-subsequence/description/
+ *
+ * algorithms
+ * Medium (67.45%)
+ * Likes:    1317
+ * Dislikes: 0
+ * Total Accepted:    294K
+ * Total Submissions: 435.8K
+ * Testcase Example:  '"bbbab"'
+ *
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ *
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "bbbab"
+ * 输出：4
+ * 解释：一个可能的最长回文子序列为 "bbbb" 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "cbbd"
+ * 输出：2
+ * 解释：一个可能的最长回文子序列为 "bb" 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * s 仅由小写英文字母组成
+ *
+ *
+ */
+
+// @lc code=start
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < n; i++ {
+		dp[i][i] = 1
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+// @lc code=end
+
