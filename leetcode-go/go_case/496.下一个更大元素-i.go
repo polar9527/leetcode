db796ed772644b4ca2a3d9553ab33e5f78210e2a
polar <polar9527@163.com>
2025-04-04 08:43:11 +0800

    monotony stack

--- leetcode-go/go_case/496.下一个更大元素-i.go
+++ leetcode-go/go_case/496.下一个更大元素-i.go
@@ -64,93 +64,129 @@ package go_case
  */
 
 // @lc code=start
-func nextGreaterElement(nums1, nums2 []int) []int {
-	// 维护一个栈，表示“待确定NGE (Next Greater Element) 的元素”，然后遍历序列。
-	// 当我们碰上一个新元素，我们知道，越靠近栈顶的元素离新元素位置越近。
-	// 所以不断比较新元素与栈顶，如果新元素比栈顶大，则可断定新元素就是栈顶的NGE，于是弹出栈顶并继续比较。
-	// 直到新元素不比栈顶大，再将新元素压入栈。显然，这样形成的栈是单调递减的。
+// func nextGreaterElement(nums1, nums2 []int) []int {
+// 	// 维护一个栈，表示“待确定NGE (Next Greater Element) 的元素”，然后遍历序列。
+// 	// 当我们碰上一个新元素，我们知道，越靠近栈顶的元素离新元素位置越近。
+// 	// 所以不断比较新元素与栈顶，如果新元素比栈顶大，则可断定新元素就是栈顶的NGE，于是弹出栈顶并继续比较。
+// 	// 直到新元素不比栈顶大，再将新元素压入栈。显然，这样形成的栈是单调递减的。
 
-	mp := map[int]int{}
-	stack := []int{}
-	for i := len(nums2) - 1; i >= 0; i-- {
-		num := nums2[i]
-		// -------  A --------------
-		// 不断拿栈顶元素与当前遍历到的元素做比较
-		// 清除掉栈内存在的比当前遍历到的元素小的栈内元素
-		// 直到栈为空，或者在栈内遇到比当前遍历到的元素 大的 栈内元素
-		for len(stack) > 0 && num > stack[len(stack)-1] {
-			// 此处 栈内元素全部是 单调递减的，栈底元素离当前所遍历到的元素nums[i]最远，栈顶元素最近
-			stack = stack[:len(stack)-1]
-		}
-		// -------  A --------------
+// 	mp := map[int]int{}
+// 	stack := []int{}
+// 	for i := len(nums2) - 1; i >= 0; i-- {
+// 		num := nums2[i]
+// 		// -------  A --------------
+// 		// 不断拿栈顶元素与当前遍历到的元素做比较
+// 		// 清除掉栈内存在的比当前遍历到的元素小的栈内元素
+// 		// 直到栈为空，或者在栈内遇到比当前遍历到的元素 大的 栈内元素
+// 		for len(stack) > 0 && num > stack[len(stack)-1] {
+// 			// 此处 栈内元素全部是 单调递减的，栈底元素离当前所遍历到的元素nums[i]最远，栈顶元素最近
+// 			stack = stack[:len(stack)-1]
+// 		}
+// 		// -------  A --------------
 
-		// -------  B --------------
-		// 根据题型，做不同处理
-		// 此处 当清理完成栈内元素之后，当前的栈顶元素（如果有的话）就是 NGE (Next Greater Element)
-		if len(stack) > 0 {
-			mp[num] = stack[len(stack)-1]
-		} else {
-			mp[num] = -1
-		}
-		// -------  B --------------
+// 		// -------  B --------------
+// 		// 根据题型，做不同处理
+// 		// 此处 当清理完成栈内元素之后，当前的栈顶元素（如果有的话）就是 NGE (Next Greater Element)
+// 		if len(stack) > 0 {
+// 			mp[num] = stack[len(stack)-1]
+// 		} else {
+// 			mp[num] = -1
+// 		}
+// 		// -------  B --------------
 
-		// -------  A --------------
-		// 清理干净 栈内元素后，将当前遍历到的元素入栈
+// 		// -------  A --------------
+// 		// 清理干净 栈内元素后，将当前遍历到的元素入栈
 
-		stack = append(stack, num)
-		// -------  A --------------
-	}
-	res := make([]int, len(nums1))
-	for i, num := range nums1 {
-		res[i] = mp[num]
-	}
-	return res
-}
+// 		stack = append(stack, num)
+// 		// -------  A --------------
+// 	}
+// 	res := make([]int, len(nums1))
+// 	for i, num := range nums1 {
+// 		res[i] = mp[num]
+// 	}
+// 	return res
+// }
 
 // 反向遍历
-func nextGreaterElementR(nums1, nums2 []int) []int {
-	// 维护一个栈，表示“待确定NGE (Next Greater Element) 的元素”，然后遍历序列。
-	// 当我们碰上一个新元素，我们知道，越靠近栈顶的元素离新元素位置越近。
-	// 所以不断比较新元素与栈顶，如果新元素比栈顶大，则可断定新元素就是栈顶的NGE，于是弹出栈顶并继续比较。
-	// 直到新元素不比栈顶大，再将新元素压入栈。显然，这样形成的栈是单调递减的。
+// func nextGreaterElementR(nums1, nums2 []int) []int {
+// 	// 维护一个栈，表示“待确定NGE (Next Greater Element) 的元素”，然后遍历序列。
+// 	// 当我们碰上一个新元素，我们知道，越靠近栈顶的元素离新元素位置越近。
+// 	// 所以不断比较新元素与栈顶，如果新元素比栈顶大，则可断定新元素就是栈顶的NGE，于是弹出栈顶并继续比较。
+// 	// 直到新元素不比栈顶大，再将新元素压入栈。显然，这样形成的栈是单调递减的。
 
-	mp := map[int]int{}
-	// for _, num := range nums2 {
-	// 	mp[num] = -1
-	// }
-	stack := []int{}
-	for i := 0; i < len(nums2); i++ {
-		num := nums2[i]
-		// -------  A --------------
-		// 不断拿栈顶元素与当前遍历到的元素做比较
-		// 清除掉栈内存在的比当前遍历到的元素小的栈内元素
-		// 直到栈为空，或者在栈内遇到比当前遍历到的元素 大的 栈内元素
-		for len(stack) > 0 && num > stack[len(stack)-1] {
-			// -------  B --------------
-			// 根据题型，做不同处理
-			// 此处 栈内元素全部是 单调递减的，栈底元素离当前所遍历到的元素nums[i]最远，栈顶元素最近
-			// 也就是说存储的都是已经遍历过的，并且都还没有找到自己右侧比自己大的第一个元素NGE
-			// 记录当前 遍历到的元素 是当前栈顶元素的右侧的第一个最大元素NGE (Next Greater Element)
-			mp[stack[len(stack)-1]] = num
-			// -------  B --------------
+// 	mp := map[int]int{}
+// 	// for _, num := range nums2 {
+// 	// 	mp[num] = -1
+// 	// }
+// 	stack := []int{}
+// 	for i := 0; i < len(nums2); i++ {
+// 		num := nums2[i]
+// 		// -------  A --------------
+// 		// 不断拿栈顶元素与当前遍历到的元素做比较
+// 		// 清除掉栈内存在的比当前遍历到的元素小的栈内元素
+// 		// 直到栈为空，或者在栈内遇到比当前遍历到的元素 大的 栈内元素
+// 		for len(stack) > 0 && num > stack[len(stack)-1] {
+// 			// -------  B --------------
+// 			// 根据题型，做不同处理
+// 			// 此处 栈内元素全部是 单调递减的，栈底元素离当前所遍历到的元素nums[i]最远，栈顶元素最近
+// 			// 也就是说存储的都是已经遍历过的，并且都还没有找到自己右侧比自己大的第一个元素NGE
+// 			// 记录当前 遍历到的元素 是当前栈顶元素的右侧的第一个最大元素NGE (Next Greater Element)
+// 			mp[stack[len(stack)-1]] = num
+// 			// -------  B --------------
 
-			stack = stack[:len(stack)-1]
-		}
-		// -------  A --------------
-		// -------  A --------------
-		// 清理干净 栈内元素后，将当前遍历到的元素入栈
+// 			stack = stack[:len(stack)-1]
+// 		}
+// 		// -------  A --------------
+// 		// -------  A --------------
+// 		// 清理干净 栈内元素后，将当前遍历到的元素入栈
 
-		stack = append(stack, num)
-		// -------  A --------------
-	}
+// 		stack = append(stack, num)
+// 		// -------  A --------------
+// 	}
+// 	res := make([]int, len(nums1))
+// 	for i, num := range nums1 {
+// 		if v, ok := mp[num]; ok {
+// 			res[i] = v
+// 		} else {
+// 			res[i] = -1
+// 		}
+
+// 	}
+// 	return res
+// }
+
+func nextGreaterElement(nums1, nums2 []int) []int {
 	res := make([]int, len(nums1))
-	for i, num := range nums1 {
-		if v, ok := mp[num]; ok {
-			res[i] = v
-		} else {
-			res[i] = -1
-		}
+	for i := range res {
+		res[i] = -1
+	}
 
+	set1 := make(map[int]int)
+
+	for i := range nums1 {
+		set1[nums1[i]] = i
+	}
+	// 通过 set2[nums1[i]] 定位 nums2 中的index
+
+	stack := []int{}
+	stack = append(stack, 0)
+
+	for i := 1; i < len(nums2); i++ {
+		if nums2[stack[len(stack)-1]] >= nums2[i] {
+			stack = append(stack, i)
+		} else {
+			// 栈顶存储的 nums2 中的 index = stack[len(stack)-1]
+			// 当前的 nums2[i] 是 nums2[stack[len(stack)-1]] 的右边首个最大值
+			for len(stack) != 0 && nums2[stack[len(stack)-1]] < nums2[i] {
+				// collect result
+				// 找到 nums1 中对应的 index
+				if index, ok := set1[nums2[stack[len(stack)-1]]]; ok {
+					res[index] = nums2[i]
+				}
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, i)
+		}
 	}
 	return res
 }
