4dc08709f7c9f35d62cce83e368e29636f433984
polar <polar9527@163.com>
2025-04-04 22:51:23 +0800

    monotony stack

--- leetcode-go/go_case/84.柱状图中最大的矩形.go
+++ leetcode-go/go_case/84.柱状图中最大的矩形.go
@@ -43,35 +43,35 @@ package go_case
 // @lc code=start
 
 // 单调栈
-func largestRectangleArea(heights []int) int {
-	max := 0
-	// 使用切片实现栈
-	stack := make([]int, 0)
-	// 数组头部加入0
-	heights = append([]int{0}, heights...)
-	// 数组尾部加入0
-	heights = append(heights, 0)
-	// 初始化栈，序号从0开始
-	stack = append(stack, 0)
-	for i := 1; i < len(heights); i++ {
-		// 结束循环条件为：当即将入栈元素>top元素，也就是形成非单调递增的趋势
-		for heights[stack[len(stack)-1]] > heights[i] {
-			// mid 是top
-			mid := stack[len(stack)-1]
-			// 出栈
-			stack = stack[0 : len(stack)-1]
-			// left是top的下一位元素，i是将要入栈的元素
-			left := stack[len(stack)-1]
-			// 高度x宽度
-			tmp := heights[mid] * (i - left - 1)
-			if tmp > max {
-				max = tmp
-			}
-		}
-		stack = append(stack, i)
-	}
-	return max
-}
+// func largestRectangleArea(heights []int) int {
+// 	max := 0
+// 	// 使用切片实现栈
+// 	stack := make([]int, 0)
+// 	// 数组头部加入0
+// 	heights = append([]int{0}, heights...)
+// 	// 数组尾部加入0
+// 	heights = append(heights, 0)
+// 	// 初始化栈，序号从0开始
+// 	stack = append(stack, 0)
+// 	for i := 1; i < len(heights); i++ {
+// 		// 结束循环条件为：当即将入栈元素>top元素，也就是形成非单调递增的趋势
+// 		for heights[stack[len(stack)-1]] > heights[i] {
+// 			// mid 是top
+// 			mid := stack[len(stack)-1]
+// 			// 出栈
+// 			stack = stack[0 : len(stack)-1]
+// 			// left是top的下一位元素，i是将要入栈的元素
+// 			left := stack[len(stack)-1]
+// 			// 高度x宽度
+// 			tmp := heights[mid] * (i - left - 1)
+// 			if tmp > max {
+// 				max = tmp
+// 			}
+// 		}
+// 		stack = append(stack, i)
+// 	}
+// 	return max
+// }
 
 // 双指针
 // func largestRectangleArea(heights []int) int {
@@ -107,5 +107,40 @@ func largestRectangleArea(heights []int) int {
 // 	}
 // 	return ans
 // }
+
+// monotony stack
+func largestRectangleArea(heights []int) int {
+
+	if len(heights) == 1 {
+		return heights[0]
+	}
+	heights = append([]int{0}, heights...)
+	heights = append(heights, 0)
+	n := len(heights)
+
+	stack := []int{0}
+	res := 0
+	for i := 1; i < n; i++ {
+		if heights[stack[len(stack)-1]] < heights[i] {
+			stack = append(stack, i)
+		} else if heights[stack[len(stack)-1]] == heights[i] {
+			stack = stack[:len(stack)-1]
+			stack = append(stack, i)
+		} else {
+			// heights[stack[len(stack)-1]] > heights[i]
+			for len(stack) != 0 && heights[stack[len(stack)-1]] > heights[i] {
+				h := heights[stack[len(stack)-1]]
+				stack = stack[:len(stack)-1]
+				if len(stack) != 0 {
+					w := i - stack[len(stack)-1] - 1
+					res = max(h*w, res)
+				}
+
+			}
+			stack = append(stack, i)
+		}
+	}
+	return res
+}
 
 // @lc code=end
