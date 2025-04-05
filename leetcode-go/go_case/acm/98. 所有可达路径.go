ac374979003fc2cc165e99d36cbd5702ed71aa28
polar <polar9527@163.com>
2025-04-05 21:45:00 +0800

    graph

--- leetcode-go/go_case/acm/98. 所有可达路径.go
+++ leetcode-go/go_case/acm/98. 所有可达路径.go
@@ -5,37 +5,88 @@ import (
 	"strings"
 )
 
-func main() {
-	// vertex numbers, edge numbers
-	var vn, en int
-	fmt.Scan(&vn, &en)
+// Adjacency Matrix
+// func main() {
+// 	// vertex numbers, edge numbers
+// 	var vn, en int
+// 	fmt.Scan(&vn, &en)
 
-	// adjacency matrix
-	// vertex -> [1, n], so vn+1
-	am := make([][]int, vn+1)
-	for i := range am {
-		am[i] = make([]int, vn+1)
+// 	// adjacency matrix
+// 	// vertex -> [1, n], so vn+1
+// 	am := make([][]int, vn+1)
+// 	for i := range am {
+// 		am[i] = make([]int, vn+1)
+// 	}
+
+// 	for en > 0 {
+// 		var out, in int
+// 		fmt.Scan(&out, &in)
+// 		am[out][in] = 1
+// 		en--
+// 	}
+
+// 	var results [][]int
+// 	var dfs func(int, []int)
+// 	dfs = func(start int, path []int) {
+// 		if start == vn {
+// 			results = append(results, append([]int{}, path...))
+// 			return
+// 		}
+// 		for next := range am[start] {
+// 			if am[start][next] == 1 {
+// 				dfs(next, append(path, next))
+// 			}
+
+// 		}
+// 	}
+
+// 	dfs(1, []int{1})
+
+// 	if len(results) == 0 {
+// 		fmt.Println(-1)
+// 	} else {
+// 		for _, result := range results {
+// 			var s []string
+// 			for _, num := range result {
+// 				s = append(s, fmt.Sprint(num))
+// 			}
+// 			fmt.Println(strings.Join(s, " "))
+// 		}
+// 	}
+
+// }
+
+// Adjacency List
+func main() {
+
+	var vn, en int
+	_, err := fmt.Scan(&vn, &en)
+	if err != nil {
+		return
 	}
+	al := make([][]int, vn+1)
 
 	for en > 0 {
 		var out, in int
 		fmt.Scan(&out, &in)
-		am[out][in] = 1
+		if len(al[out]) == 0 {
+			al[out] = []int{in}
+		} else {
+			al[out] = append(al[out], in)
+		}
 		en--
 	}
 
 	var results [][]int
+
 	var dfs func(int, []int)
 	dfs = func(start int, path []int) {
 		if start == vn {
 			results = append(results, append([]int{}, path...))
 			return
 		}
-		for next := range am[start] {
-			if am[start][next] == 1 {
-				dfs(next, append(path, next))
-			}
-
+		for _, next := range al[start] {
+			dfs(next, append(path, next))
 		}
 	}
 
@@ -43,14 +94,14 @@ func main() {
 
 	if len(results) == 0 {
 		fmt.Println(-1)
-	} else {
-		for _, result := range results {
-			var s []string
-			for _, num := range result {
-				s = append(s, fmt.Sprint(num))
-			}
-			fmt.Println(strings.Join(s, " "))
+		return
+	}
+	for _, p := range results {
+		var s []string
+		for _, vertex := range p {
+			s = append(s, fmt.Sprint(vertex))
 		}
+		fmt.Println(strings.Join(s, " "))
 	}
 
 }
