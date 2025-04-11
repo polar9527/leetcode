6a2a20853c167bc4c4d0e71aa7dbb231f014f09d
polar <polar9527@163.com>
2025-04-11 18:09:26 +0800

    disjoint set

--- leetcode-go/go_case/685.冗余连接-ii.go
+++ leetcode-go/go_case/685.冗余连接-ii.go
@@ -58,65 +58,160 @@ package go_case
 // @lc code=start
 // 全局变量
 
-func findRedundantDirectedConnection(edges [][]int) []int {
+// func findRedundantDirectedConnection(edges [][]int) []int {
 
+// 	n := len(edges)
+// 	father := make([]int, n+1)
+
+// 	// 并查集初始化
+// 	initialize := func() {
+// 		for i := 1; i < n+1; i++ {
+// 			father[i] = i
+// 		}
+// 	}
+
+// 	// 并查集里寻根的过程
+// 	var find func(int) int
+// 	find = func(u int) int {
+// 		if u == father[u] {
+// 			return u
+// 		}
+// 		father[u] = find(father[u])
+// 		return father[u]
+// 	}
+
+// 	// 将v->u 这条边加入并查集
+// 	join := func(u, v int) {
+// 		u = find(u)
+// 		v = find(v)
+// 		if u == v {
+// 			return
+// 		}
+// 		father[v] = u
+// 	}
+
+// 	// 判断 u 和 v是否找到同一个根，本题用不上
+// 	same := func(u, v int) bool {
+// 		u = find(u)
+// 		v = find(v)
+// 		return u == v
+// 	}
+
+// 	// getRemoveEdge 在有向图里找到删除的那条边，使其变成树
+// 	getRemoveEdge := func(edges [][]int) []int {
+// 		initialize()
+// 		for i := 0; i < len(edges); i++ { // 遍历所有的边
+// 			if same(edges[i][0], edges[i][1]) { // 构成有向环了，就是要删除的边
+// 				return edges[i]
+// 			}
+// 			join(edges[i][0], edges[i][1])
+// 		}
+// 		return []int{}
+// 	}
+
+// 	// isTreeAfterRemoveEdge 删一条边之后判断是不是树
+// 	isTreeAfterRemoveEdge := func(edges [][]int, deleteEdge int) bool {
+// 		initialize()
+// 		for i := 0; i < len(edges); i++ {
+// 			if i == deleteEdge {
+// 				continue
+// 			}
+// 			if same(edges[i][0], edges[i][1]) { // 构成有向环了，一定不是树
+// 				return false
+// 			}
+// 			join(edges[i][0], edges[i][1])
+// 		}
+// 		return true
+// 	}
+
+// 	inDegree := make([]int, len(father))
+// 	for i := 0; i < len(edges); i++ {
+// 		// 统计入度
+// 		inDegree[edges[i][1]] += 1
+// 	}
+// 	// 记录入度为2的边（如果有的话就两条边）
+// 	// 找入度为2的节点所对应的边，注意要倒序，因为优先返回最后出现在二维数组中的答案
+// 	twoDegree := make([]int, 0)
+// 	for i := len(edges) - 1; i >= 0; i-- {
+// 		if inDegree[edges[i][1]] == 2 {
+// 			twoDegree = append(twoDegree, i)
+// 		}
+// 	}
+
+// 	// 处理图中情况1 和 情况2
+// 	// 如果有入度为2的节点，那么一定是两条边里删一个，看删哪个可以构成树
+// 	if len(twoDegree) > 0 {
+// 		if isTreeAfterRemoveEdge(edges, twoDegree[0]) {
+// 			return edges[twoDegree[0]]
+// 		}
+// 		return edges[twoDegree[1]]
+// 	}
+
+// 	// 处理图中情况3
+// 	// 明确没有入度为2的情况，那么一定有有向环，找到构成环的边返回就可以了
+// 	return getRemoveEdge(edges)
+// }
+
+func findRedundantDirectedConnection(edges [][]int) (redundantEdge []int) {
+	// 树有 len(edges)-1 个边，再加上一个 附加边， 和 len(edges)个节点
 	n := len(edges)
-	father := make([]int, n+1)
-
-	// 并查集初始化
-	initialize := func() {
-		for i := 1; i < n+1; i++ {
-			father[i] = i
+	ancestor := make([]int, n+1)
+	initial := func() {
+		for i := 1; i <= n; i++ {
+			ancestor[i] = i
 		}
 	}
-
-	// 并查集里寻根的过程
 	var find func(int) int
-	find = func(u int) int {
-		if u == father[u] {
-			return u
+	find = func(node int) int {
+		if node == ancestor[node] {
+			return ancestor[node]
 		}
-		father[u] = find(father[u])
-		return father[u]
+		// 压缩
+		nodeRoot := find(ancestor[node])
+		ancestor[node] = nodeRoot
+		return nodeRoot
 	}
 
-	// 将v->u 这条边加入并查集
-	join := func(u, v int) {
-		u = find(u)
-		v = find(v)
-		if u == v {
-			return
+	isSame := func(so, fa int) bool {
+		soRoot := find(so)
+		faRoot := find(fa)
+
+		return faRoot == soRoot
+	}
+
+	join := func(so, fa int) {
+		soRoot := find(so)
+		faRoot := find(fa)
+		if soRoot != faRoot {
+			ancestor[soRoot] = faRoot
 		}
-		father[v] = u
 	}
 
-	// 判断 u 和 v是否找到同一个根，本题用不上
-	same := func(u, v int) bool {
-		u = find(u)
-		v = find(v)
-		return u == v
+	inDegree := make([]int, n+1)
+	// 统计各节点入度
+	// edge[fa, so]
+	// edge[0, 1]
+	for i := 0; i < n; i++ {
+		// 入度节点 是 [1]
+		inDegree[edges[i][1]]++
 	}
 
-	// getRemoveEdge 在有向图里找到删除的那条边，使其变成树
-	getRemoveEdge := func(edges [][]int) []int {
-		initialize()
-		for i := 0; i < len(edges); i++ { // 遍历所有的边
-			if same(edges[i][0], edges[i][1]) { // 构成有向环了，就是要删除的边
-				return edges[i]
-			}
-			join(edges[i][0], edges[i][1])
+	// 找到入度为2 的 节点node, 所对应的边edge
+	edgeIndex := make([]int, 0)
+	for i := n - 1; i >= 0; i-- {
+		// 入度节点 是 [1]
+		if inDegree[edges[i][1]] == 2 {
+			edgeIndex = append(edgeIndex, i)
 		}
-		return []int{}
 	}
 
-	// isTreeAfterRemoveEdge 删一条边之后判断是不是树
-	isTreeAfterRemoveEdge := func(edges [][]int, deleteEdge int) bool {
-		initialize()
-		for i := 0; i < len(edges); i++ {
-			if i == deleteEdge {
+	isTree := func(edgeDeleted int) bool {
+		initial()
+		for i := 0; i < n; i++ {
+			if i == edgeDeleted {
 				continue
 			}
-			if same(edges[i][0], edges[i][1]) { // 构成有向环了，一定不是树
+			if isSame(edges[i][0], edges[i][1]) {
 				return false
 			}
 			join(edges[i][0], edges[i][1])
@@ -124,32 +219,30 @@ func findRedundantDirectedConnection(edges [][]int) []int {
 		return true
 	}
 
-	inDegree := make([]int, len(father))
-	for i := 0; i < len(edges); i++ {
-		// 统计入度
-		inDegree[edges[i][1]] += 1
-	}
-	// 记录入度为2的边（如果有的话就两条边）
-	// 找入度为2的节点所对应的边，注意要倒序，因为优先返回最后出现在二维数组中的答案
-	twoDegree := make([]int, 0)
-	for i := len(edges) - 1; i >= 0; i-- {
-		if inDegree[edges[i][1]] == 2 {
-			twoDegree = append(twoDegree, i)
+	// 添加的 有向边 没有 指向树的根节点，导致有一个节点入度为2
+	if len(edgeIndex) > 0 {
+		// 判断 edgeIndex 中的哪条边是 不在树中的 边
+		if isTree(edgeIndex[0]) {
+			return edges[edgeIndex[0]]
 		}
+		return edges[edgeIndex[1]]
 	}
 
-	// 处理图中情况1 和 情况2
-	// 如果有入度为2的节点，那么一定是两条边里删一个，看删哪个可以构成树
-	if len(twoDegree) > 0 {
-		if isTreeAfterRemoveEdge(edges, twoDegree[0]) {
-			return edges[twoDegree[0]]
+	getTheEdge := func() []int {
+		initial()
+		var res []int
+		// 按顺序遍历，最后遇到的导致成环的边就是结果
+		for i := 0; i < n; i++ {
+			if isSame(edges[i][0], edges[i][1]) {
+				res = edges[i]
+			}
+			join(edges[i][0], edges[i][1])
 		}
-		return edges[twoDegree[1]]
+		return res
 	}
+	//添加的 有向边 指向树的根节点，导致所有一个节点入度均为1
 
-	// 处理图中情况3
-	// 明确没有入度为2的情况，那么一定有有向环，找到构成环的边返回就可以了
-	return getRemoveEdge(edges)
+	return getTheEdge()
 }
 
 // @lc code=end
