ef854580e1eed625eb334a2b5f20fbcd1c575016
polar <polar9527@163.com>
2025-04-14 07:33:42 +0800

    greedy

--- leetcode-go/go_case/435.无重叠区间.go
+++ leetcode-go/go_case/435.无重叠区间.go
@@ -60,23 +60,18 @@
 func eraseOverlapIntervals(intervals [][]int) int {
 
 	sort.Slice(intervals, func(i, j int) bool {
-		if intervals[i][0] == intervals[j][0] {
-			return intervals[i][1] < intervals[j][1]
-		}
-
-		return intervals[i][0] < intervals[j][0]
+		return intervals[i][1] < intervals[j][1]
 	})
-	res := 0
+	count := 1
+	end := intervals[0][1]
 	for i := 1; i < len(intervals); i++ {
-		if intervals[i][0] < intervals[i-1][1] {
-			res++
-			if intervals[i][1] > intervals[i-1][1] {
-				intervals[i][1] = intervals[i-1][1]
-			}
-
+		if end <= intervals[i][0] {
+			// 无重叠的线段
+			count++
+			end = intervals[i][1]
 		}
 	}
-	return res
+	return len(intervals) - count
 }
 
 // @lc code=end
