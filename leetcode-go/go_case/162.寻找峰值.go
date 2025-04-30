680df5b6b7a2a3d089932457b6403457306678b1
polar <polar9527@163.com>
2025-05-01 01:43:24 +0800

    binary search

--- leetcode-go/go_case/162.寻找峰值.go
+++ leetcode-go/go_case/162.寻找峰值.go
@@ -1,7 +1,5 @@
 package go_case
 
-import "math"
-
 /*
  * @lc app=leetcode.cn id=162 lang=golang
  *
@@ -57,30 +55,56 @@ import "math"
 
 // @lc code=start
 
+// func findPeakElement(nums []int) int {
+// 	n := len(nums)
+
+// 	// 辅助函数，输入下标 i，返回 nums[i] 的值
+// 	// 方便处理 nums[-1] 以及 nums[n] 的边界情况
+// 	get := func(i int) int {
+// 		if i == -1 || i == n {
+// 			return math.MinInt64
+// 		}
+// 		return nums[i]
+// 	}
+
+// 	left, right := 0, n-1
+// 	for {
+// 		mid := (left + right) / 2
+// 		if get(mid-1) < get(mid) && get(mid) > get(mid+1) {
+// 			return mid
+// 		}
+// 		if get(mid) < get(mid+1) {
+// 			left = mid + 1
+// 		} else {
+// 			right = mid
+// 		}
+// 	}
+// }
+
 func findPeakElement(nums []int) int {
 	n := len(nums)
-
-	// 辅助函数，输入下标 i，返回 nums[i] 的值
-	// 方便处理 nums[-1] 以及 nums[n] 的边界情况
-	get := func(i int) int {
-		if i == -1 || i == n {
-			return math.MinInt64
-		}
-		return nums[i]
+	if n == 1 {
+		return 0
 	}
 
-	left, right := 0, n-1
-	for {
-		mid := (left + right) / 2
-		if get(mid-1) < get(mid) && get(mid) > get(mid+1) {
-			return mid
-		}
-		if get(mid) < get(mid+1) {
-			left = mid + 1
+	// 这样后面的 mid+1 就不会越界
+	l, r := 0, n-2
+
+	// [l,r]
+	for l <= r {
+		mid := l + (r-l)>>1
+
+		if nums[mid] < nums[mid+1] {
+			// 而且当 mid + 1 == n-1 时
+			// l = n-1, 意味着这就是峰值，循环结束
+			// 并以 l 为结果返回
+			l = mid + 1
 		} else {
-			right = mid - 1
+			// nums[mid] > nums[mid+1]
+			r = mid - 1
 		}
 	}
+	return l
 }
 
 // @lc code=end
