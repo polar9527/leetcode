c0bbeeade7676e06f1528b521ccedf76935a7013
polar <polar9527@163.com>
2025-05-07 14:50:01 +0800

    list

---
+++ leetcode-go/go_case/82.删除排序链表中的重复元素-ii.go
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=82 lang=golang
+ *
+ * [82] 删除排序链表中的重复元素 II
+ *
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/description/
+ *
+ * algorithms
+ * Medium (54.88%)
+ * Likes:    1367
+ * Dislikes: 0
+ * Total Accepted:    530.7K
+ * Total Submissions: 964.3K
+ * Testcase Example:  '[1,2,3,3,4,4,5]'
+ *
+ * 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,1,1,2,3]
+ * 输出：[2,3]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序 排列
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		val := cur.Next.Val
+		if val == cur.Next.Next.Val {
+			for cur.Next != nil && cur.Next.Val == val {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummy.Next
+}
+
+// @lc code=end
+
