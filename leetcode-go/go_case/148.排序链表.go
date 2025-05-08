a5cb4754562864f61721f2cf0cd0c23907dd749d
polar <polar9527@163.com>
2025-05-08 15:58:00 +0800

    list

--- leetcode-go/go_case/148.排序链表.go
+++ leetcode-go/go_case/148.排序链表.go
@@ -66,50 +66,51 @@ package go_case
  *     Next *ListNode
  * }
  */
-func sortList(head *ListNode) *ListNode {
-	return sort(head, nil)
-}
-
-func sortL(h, t *ListNode) *ListNode {
-	if h == nil {
-		return h
-	}
-	if h.Next == t {
-		h.Next = nil
-		return h
-	}
-	slow, fast := h, h
-	for fast != t {
+// 876. 链表的中间结点（快慢指针）
+func middleNode(head *ListNode) *ListNode {
+	pre, slow, fast := head, head, head
+	for fast != nil && fast.Next != nil {
+		pre = slow // 记录 slow 的前一个节点
 		slow = slow.Next
-		fast = fast.Next
-		if fast != t {
-			fast = fast.Next
-		}
+		fast = fast.Next.Next
 	}
-	mid := slow
-	return mergeL(sortL(h, mid), sortL(mid, t))
+
+	pre.Next = nil // 断开 slow 的前一个节点和 slow 的连接
+
+	return slow
 }
 
-func mergeL(head1, head2 *ListNode) *ListNode {
-	dummyHead := &ListNode{}
-	temp, l, r := dummyHead, head1, head2
-	for l != nil && r != nil {
-		if l.Val < r.Val {
-			temp.Next = l
-			l = l.Next
-		} else {
-			temp.Next = r
-			r = r.Next
+// 21. 合并两个有序链表（双指针）
+func mergeTwoLists(list1, list2 *ListNode) *ListNode {
+	dummy := ListNode{} // 用哨兵节点简化代码逻辑
+	cur := &dummy       // cur 指向新链表的末尾
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1 // 把 list1 加到新链表中
+			list1 = list1.Next
+		} else { // 注：相等的情况加哪个节点都是可以的
+			cur.Next = list2 // 把 list2 加到新链表中
+			list2 = list2.Next
 		}
-		temp = temp.Next
+		cur = cur.Next
 	}
-	if l != nil {
-		temp.Next = l
+	// 拼接剩余链表
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
 	}
-	if r != nil {
-		temp.Next = r
+	return dummy.Next
+}
+
+func sortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
 	}
-	return dummyHead.Next
+	head2 := middleNode(head) // head2 的前一个节点和 head2 的连接 已经断开
+	head = sortList(head)
+	head2 = sortList(head2)
+	return mergeTwoLists(head, head2)
 }
 
 // @lc code=end
