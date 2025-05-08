5dea7d70fd381c230b66454e62d3f004adace76d
polar <polar9527@163.com>
2025-05-08 15:57:55 +0800

    list

--- leetcode-go/go_case/21.合并两个有序链表.go
+++ leetcode-go/go_case/21.合并两个有序链表.go
@@ -58,27 +58,45 @@
  *     Next *ListNode
  * }
  */
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	dummy := &ListNode{}
-	cur := dummy
-	for list1 != nil && list2 != nil {
-		if list1.Val < list2.Val {
-			cur.Next = list1
-			list1 = list1.Next
-		} else {
-			cur.Next = list2
-			list2 = list2.Next
-		}
-		cur = cur.Next
-	}
-	if list1 != nil {
-		cur.Next = list1
-	}
-	if list2 != nil {
-		cur.Next = list2
-	}
+// iterative
+// func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+// 	dummy := &ListNode{}
+// 	cur := dummy
+// 	for list1 != nil && list2 != nil {
+// 		if list1.Val < list2.Val {
+// 			cur.Next = list1
+// 			list1 = list1.Next
+// 		} else {
+// 			cur.Next = list2
+// 			list2 = list2.Next
+// 		}
+// 		cur = cur.Next
+// 	}
+// 	if list1 != nil {
+// 		cur.Next = list1
+// 	}
+// 	if list2 != nil {
+// 		cur.Next = list2
+// 	}
 
-	return dummy.Next
+// 	return dummy.Next
+// }
+
+// recursive
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+	list2.Next = mergeTwoLists(list1, list2.Next)
+	return list2
+
 }
 
 // @lc code=end
