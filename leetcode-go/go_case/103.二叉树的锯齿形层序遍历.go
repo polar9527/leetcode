64a21868c54b9e47810b296ad7c2d3fa3319c400
polar <polar9527@163.com>
2025-05-11 23:36:30 +0800

    tree

---
+++ leetcode-go/go_case/103.二叉树的锯齿形层序遍历.go
@@ -0,0 +1,93 @@
+5/*
+ * @lc app=leetcode.cn id=103 lang=golang
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (59.96%)
+ * Likes:    960
+ * Dislikes: 0
+ * Total Accepted:    446.9K
+ * Total Submissions: 742.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[20,9],[15,7]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 2000] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	var res [][]int
+	var queue []*TreeNode
+	queue = append(queue, root)
+	for len(queue) != 0 {
+		n := len(queue)
+		level := make([]int, n)
+		for i := 0; i < n; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if len(res) % 2 > 0 {
+				level[n-i-1] = node.Val
+			} else {
+				level[i] = node.Val
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, level)
+
+	}
+	return res
+}
+// @lc code=end
+
