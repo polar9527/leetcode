57b25f1c94f49e777423848dbd0bf5b64a6ca00b
polar <polar9527@163.com>
2025-05-12 15:01:55 +0800

    tree

---
+++ leetcode-go/go_case/543.二叉树的直径.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=golang
+ *
+ * [543] 二叉树的直径
+ *
+ * https://leetcode.cn/problems/diameter-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (62.24%)
+ * Likes:    1722
+ * Dislikes: 0
+ * Total Accepted:    590.8K
+ * Total Submissions: 941.9K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你一棵二叉树的根节点，返回该树的 直径 。
+ *
+ * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+ *
+ * 两节点之间路径的 长度 由它们之间边数表示。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,4,5]
+ * 输出：3
+ * 解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [1, 10^4] 内
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) int {
+	var res int
+	var dfs func(*TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return -1
+		}
+		lLen := dfs(node.Left) + 1
+		rLen := dfs(node.Right) + 1
+		res = max(lLen+rLen, res)
+		return max(lLen, rLen)
+	}
+	dfs(root)
+	return res
+}
+
+// @lc code=end
+
