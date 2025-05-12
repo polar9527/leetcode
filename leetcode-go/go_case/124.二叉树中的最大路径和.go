56f63ce9461f4dfb4dd9630a0f93dcea984432ea
polar <polar9527@163.com>
2025-05-12 21:50:04 +0800

    tree

--- leetcode-go/go_case/124.二叉树中的最大路径和.go
+++ leetcode-go/go_case/124.二叉树中的最大路径和.go
@@ -62,36 +62,21 @@ import "math"
  * }
  */
 func maxPathSum(root *TreeNode) int {
-	maxSum := math.MinInt32
-	var maxGain func(*TreeNode) int
-	maxGain = func(node *TreeNode) int {
+	// 链和 与 路径和
+	res := math.MinInt
+	// 返回 最大链路和
+	var dfs func(*TreeNode) int
+	dfs = func(node *TreeNode) int {
 		if node == nil {
 			return 0
 		}
-
-		// 递归计算左右子节点的最大贡献值
-		// 只有在最大贡献值大于 0 时，才会选取对应子节点
-		leftGain := max(maxGain(node.Left), 0)
-		rightGain := max(maxGain(node.Right), 0)
-
-		// 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
-		priceNewPath := node.Val + leftGain + rightGain
-
-		// 更新答案
-		maxSum = max(maxSum, priceNewPath)
-
-		// 返回节点的最大贡献值
-		return node.Val + max(leftGain, rightGain)
+		lMax := dfs(node.Left)
+		rMax := dfs(node.Right)
+		res = max(res, lMax+rMax+node.Val)
+		return max(0, max(lMax, rMax)+node.Val)
 	}
-	maxGain(root)
-	return maxSum
+	dfs(root)
+	return res
 }
-
-// func max(x, y int) int {
-// 	if x > y {
-// 		return x
-// 	}
-// 	return y
-// }
 
 // @lc code=end
