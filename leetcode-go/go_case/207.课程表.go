742ee3b81ca0314f16d14a22a689ec2fe9d7b945
polar <polar9527@163.com>
2025-05-18 15:59:10 +0800

    graph

--- leetcode-go/go_case/207.课程表.go
+++ leetcode-go/go_case/207.课程表.go
@@ -55,46 +55,106 @@
  */
 
 // @lc code=start
-func canFinish(numCourses int, prerequisites [][]int) bool {
+// func canFinish(numCourses int, prerequisites [][]int) bool {
 
-	inDegree := make(map[int]int)
-	dependencies := make(map[int][]int)
-	dn := len(prerequisites)
-	for i := 0; i < dn; i++ {
-		to, from := prerequisites[i][0], prerequisites[i][1]
-		inDegree[to]++
-		dependencies[from] = append(dependencies[from], to)
+// 	inDegree := make(map[int]int)
+// 	dependencies := make(map[int][]int)
+// 	dn := len(prerequisites)
+// 	for i := 0; i < dn; i++ {
+// 		to, from := prerequisites[i][0], prerequisites[i][1]
+// 		inDegree[to]++
+// 		dependencies[from] = append(dependencies[from], to)
+// 	}
+
+// 	que := make([]int, 0)
+// 	for i := 0; i < numCourses; i++ {
+// 		if inDegree[i] == 0 {
+// 			que = append(que, i)
+// 		}
+// 	}
+
+// 	count := 0
+// 	for len(que) > 0 {
+// 		cur := que[0]
+// 		que = que[1:]
+// 		count++
+
+// 		files := dependencies[cur]
+// 		if len(files) != 0 {
+// 			for _, f := range files {
+// 				inDegree[f]--
+// 				if inDegree[f] == 0 {
+// 					que = append(que, f)
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return count == numCourses
+// }
+
+// 拓扑排序 优化版本，不用字典
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	inDegrees := make([]int, numCourses)
+	dependences := make([][]int, numCourses)
+	for _, prerequisit := range prerequisites {
+		// from -> to
+		to, from := prerequisit[0], prerequisit[1]
+		inDegrees[to]++
+		// from -> to1
+		// 	 	-> to2
+		// 		-> ...
+		dependences[from] = append(dependences[from], to)
 	}
 
-	que := make([]int, 0)
-	for i := 0; i < numCourses; i++ {
-		if inDegree[i] == 0 {
-			que = append(que, i)
+	queue := []int{}
+	for to, degree := range inDegrees {
+		if degree == 0 {
+			queue = append(queue, to)
 		}
 	}
-
-	res := []int{}
-	for len(que) > 0 {
-		cur := que[0]
-		que = que[1:]
-		res = append(res, cur)
-
-		files := dependencies[cur]
-		if len(files) != 0 {
-			for _, f := range files {
-				inDegree[f]--
-				if inDegree[f] == 0 {
-					que = append(que, f)
-				}
+	count := 0
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		count++
+		for _, next := range dependences[cur] {
+			inDegrees[next]--
+			if inDegrees[next] == 0 {
+				queue = append(queue, next)
 			}
 		}
 	}
-	if len(res) == numCourses {
-
-		return true
-	}
-	return false
+	return count == numCourses
 }
+
+// 环检测
+// func canFinish(numCourses int, prerequisites [][]int) bool {
+// 	g := make([][]int, numCourses)
+// 	for _, p := range prerequisites {
+// 		g[p[1]] = append(g[p[1]], p[0])
+// 	}
+
+// 	colors := make([]int, numCourses)
+// 	var dfs func(int) bool
+// 	dfs = func(x int) bool {
+// 		colors[x] = 1 // x 正在访问中
+// 		for _, y := range g[x] {
+// 			if colors[y] == 1 || colors[y] == 0 && dfs(y) {
+// 				return true // 找到了环
+// 			}
+// 		}
+// 		colors[x] = 2 // x 完全访问完毕
+// 		return false  // 没有找到环
+// 	}
+
+// 	for i, c := range colors {
+// 		if c == 0 && dfs(i) {
+// 			return false // 有环
+// 		}
+// 	}
+// 	return true // 没有环
+// }
 
 // @lc code=end
 
