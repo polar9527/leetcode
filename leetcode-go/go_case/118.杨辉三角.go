24677f09b67000e66955f56e5a25fd129106b658
polar <polar9527@163.com>
2025-05-18 22:00:43 +0800

    math

---
+++ leetcode-go/go_case/118.杨辉三角.go
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=golang
+ *
+ * [118] 杨辉三角
+ *
+ * https://leetcode.cn/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (77.20%)
+ * Likes:    1251
+ * Dislikes: 0
+ * Total Accepted:    662.5K
+ * Total Submissions: 854K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: numRows = 5
+ * 输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: numRows = 1
+ * 输出: [[1]]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+func generate(numRows int) [][]int {
+	res := make([][]int, numRows)
+
+	for i := range res {
+		res[i] = make([]int, i+1)
+		res[i][0], res[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j] + res[i-1][j-1]
+		}
+	}
+	return res
+}
+
+// @lc code=end
+
