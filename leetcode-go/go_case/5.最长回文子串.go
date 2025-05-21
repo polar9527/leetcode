f7d5c678ad9c66100f1e441f43c9f2a2fa001d96
polar <polar9527@163.com>
2025-05-21 09:39:10 +0800

    DP

--- leetcode-go/go_case/5.最长回文子串.go
+++ leetcode-go/go_case/5.最长回文子串.go
@@ -45,43 +45,126 @@
 
 // @lc code=start
 // dp
-func longestPalindrome(s string) string {
+// func longestPalindrome(s string) string {
 
+// 	n := len(s)
+// 	dp := make([][]bool, n)
+// 	for i := range dp {
+// 		dp[i] = make([]bool, n)
+// 	}
+
+// 	for i := 0; i < n; i++ {
+// 		dp[i][i] = true
+// 	}
+// 	maxlen := 0
+// 	res := [2]int{0, 0}
+// 	for i := n - 1; i >= 0; i-- {
+// 		for j := i; j < n; j++ {
+// 			if s[i] == s[j] {
+// 				if j-i <= 1 {
+// 					dp[i][j] = true
+// 				} else {
+// 					if dp[i+1][j-1] {
+// 						dp[i][j] = true
+// 					} else {
+// 						dp[i][j] = false
+// 					}
+// 				}
+// 			} else {
+// 				dp[i][j] = false
+// 			}
+// 			if dp[i][j] && (j-i+1 > maxlen) {
+// 				maxlen = j - i + 1
+// 				res = [2]int{i, j}
+// 			}
+// 		}
+// 	}
+
+// 	return s[res[0] : res[1]+1]
+
+// }
+
+// 中心扩展检测
+// func longestPalindrome(s string) string {
+// 	n := len(s)
+// 	if n == 0 {
+// 		return ""
+// 	}
+
+// 	start, end := 0, 0
+
+// 	// 定义检查回文的DFS函数
+// 	var expandAroundCenter func(left, right int) (int, int)
+// 	expandAroundCenter = func(left, right int) (int, int) {
+// 		for left >= 0 && right < n && s[left] == s[right] {
+// 			left--
+// 			right++
+// 		}
+// 		return left + 1, right - 1
+// 	}
+
+// 	for i := 0; i < n; i++ {
+// 		// 奇数长度回文
+// 		l1, r1 := expandAroundCenter(i, i)
+// 		// 偶数长度回文
+// 		l2, r2 := expandAroundCenter(i, i+1)
+
+// 		// 更新最长回文
+// 		if r1-l1 > end-start {
+// 			start, end = l1, r1
+// 		}
+// 		if r2-l2 > end-start {
+// 			start, end = l2, r2
+// 		}
+// 	}
+
+// 	return s[start : end+1]
+// }
+
+func longestPalindrome(s string) string {
 	n := len(s)
+	if n < 2 {
+		return s
+	}
+
+	// dp[i][j] 表示s[i..j]是否是回文
 	dp := make([][]bool, n)
 	for i := range dp {
 		dp[i] = make([]bool, n)
 	}
 
+	start, maxLen := 0, 1
+
+	// 所有长度为1的子串都是回文
 	for i := 0; i < n; i++ {
 		dp[i][i] = true
 	}
-	maxlen := 0
-	res := [2]int{0, 0}
-	for i := n - 1; i >= 0; i-- {
-		for j := i; j < n; j++ {
-			if s[i] == s[j] {
-				if j-i <= 1 {
-					dp[i][j] = true
-				} else {
-					if dp[i+1][j-1] {
-						dp[i][j] = true
-					} else {
-						dp[i][j] = false
-					}
+
+	// 检查长度为2的子串
+	for i := 0; i < n-1; i++ {
+		if s[i] == s[i+1] {
+			dp[i][i+1] = true
+			start = i
+			maxLen = 2
+		}
+	}
+
+	// 检查长度大于2的子串
+	for length := 3; length <= n; length++ {
+		for i := 0; i < n-length+1; i++ {
+			j := i + length - 1
+			// 首尾字符相同，且中间子串是回文
+			if s[i] == s[j] && dp[i+1][j-1] {
+				dp[i][j] = true
+				if length > maxLen {
+					start = i
+					maxLen = length
 				}
-			} else {
-				dp[i][j] = false
-			}
-			if dp[i][j] && (j-i+1 > maxlen) {
-				maxlen = j - i + 1
-				res = [2]int{i, j}
 			}
 		}
 	}
 
-	return s[res[0] : res[1]+1]
-
+	return s[start : start+maxLen]
 }
 
 // @lc code=end
