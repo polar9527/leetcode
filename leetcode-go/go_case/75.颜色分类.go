2062963d5f4d240a1c5cfd354015eca590b2f475
polar <polar9527@163.com>
2025-06-12 21:32:38 +0800

    math

---
+++ leetcode-go/go_case/75.颜色分类.go
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=75 lang=golang
+ *
+ * [75] 颜色分类
+ *
+ * https://leetcode.cn/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (62.25%)
+ * Likes:    1948
+ * Dislikes: 0
+ * Total Accepted:    799K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * 给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地 对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ *
+ * 我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ *
+ *
+ *
+ *
+ * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [2,0,2,1,1,0]
+ * 输出：[0,0,1,1,2,2]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [2,0,1]
+ * 输出：[0,1,2]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums.length
+ * 1 <= n <= 300
+ * nums[i] 为 0、1 或 2
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 你能想出一个仅使用常数空间的一趟扫描算法吗？
+ *
+ *
+ */
+
+// @lc code=start
+func sortColors(nums []int) {
+	// 排好序后
+	// 0的个数
+	c0 := 0
+	//  0 和 1 的个数
+	c1 := 0
+
+	for i, num := range nums {
+		nums[i] = 2
+		if num <= 1 {
+			nums[c1] = 1
+			c1++
+		}
+		if num == 0 {
+			nums[c0] = 0
+			c0++
+		}
+	}
+}
+
+// @lc code=end
+
